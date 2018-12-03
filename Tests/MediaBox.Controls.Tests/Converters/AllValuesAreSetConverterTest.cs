1add3af5f24cfc62e2b5dfdbf0d9f06f41b9f799
akina <testakina@gmail.com>
2018-12-03 22:22:40 +0900

    AllValuesAreSetConverterのユニットテストを追加

---
+++ Tests/MediaBox.Controls.Tests/Converters/AllValuesAreSetConverterTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using NUnit.Framework;
+using SandBeige.MediaBox.Controls.Converters;
+
+namespace SandBeige.MediaBox.Controls.Tests.Converters {
+	[TestFixture]
+	internal class AllValuesAreSetConverterTest {
+		[TestCase(false, new object[] { 1, 2, 3, "DependencyProperty.UnsetValue" })]
+		[TestCase(false, new object[] { "DependencyProperty.UnsetValue" })]
+		[TestCase(false, new object[] { null })]
+		[TestCase(false, new object[] { "DependencyProperty.UnsetValue", 1, 2, 3 })]
+		[TestCase(false, new object[] { null, 1, 2, 3 })]
+		[TestCase(false, new object[] { "a", 'c', true, null })]
+		[TestCase(false, new object[] { "a", 'c', true, "DependencyProperty.UnsetValue" })]
+		[TestCase(false, new object[] { "a", 'c', null, "DependencyProperty.UnsetValue" })]
+		[TestCase(true, new object[] { 1, 2, 3 })]
+		[TestCase(true, new object[] { "a", 'c', true })]
+		[TestCase(true, new object[] { 0 })]
+		[TestCase(true, new object[] { "" })]
+		[TestCase(true, new object[] { })]
+		public void Convert(bool result, object[] values) {
+			for (var i = 0; i < values.Length; i++) {
+				// 属性引数に定数しか入れられないので、苦肉の策
+				if (values[i] is string str && str == "DependencyProperty.UnsetValue") {
+					values[i] = DependencyProperty.UnsetValue;
+				}
+			}
+			var converter = new AllValuesAreSetConverter();
+			Assert.AreEqual(result, converter.Convert(values, typeof(bool), null, CultureInfo.InvariantCulture));
+		}
+
+		[TestCase(1)]
+		[TestCase(0)]
+		[TestCase(null)]
+		[TestCase("aa")]
+		public void ConvertBack(object value) {
+			var converter = new AllValuesAreSetConverter();
+			Assert.Throws<NotImplementedException>(() => {
+				converter.ConvertBack(value, new[] { typeof(bool) }, null, CultureInfo.InvariantCulture);
+			});
+		}
+	}
+}
---
+++ Tests/MediaBox.Controls.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MediaBox.Controls.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MediaBox.Controls.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("777fb11b-5ae0-440c-ab95-aee66481b774")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
