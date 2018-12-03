9125deae46bff1220e6af15229116d9805096cd9
akina <testakina@gmail.com>
2018-12-03 23:28:31 +0900

    LatLongToLocationConverterのユニットテストを追加

---
+++ Tests/MediaBox.Controls.Tests/Converters/LatLongToLocationConverterTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+using Microsoft.Maps.MapControl.WPF;
+using NUnit.Framework;
+using SandBeige.MediaBox.Controls.Converters;
+
+namespace SandBeige.MediaBox.Controls.Tests.Converters {
+	[TestFixture]
+	internal class LatLongToLocationConverterTest {
+		[TestCase(35d, 134d)]
+		[TestCase(34.9384983, 135.9120257)]
+		[TestCase(20000d, 190d)]
+		[TestCase(0d, 0d)]
+		[TestCase(-5d, -51d)]
+		[TestCase(180d, 180d)]
+		public void Convert(object lat, object lon) {
+			var converter = new LatLongToLocationConverter();
+			var location = (Location)converter.Convert(new[] { lat, lon }, typeof(bool), null, CultureInfo.InvariantCulture);
+			Assert.AreEqual(lat, location.Latitude);
+			Assert.AreEqual(lon, location.Longitude);
+		}
+
+		[TestCase(null, 134d)]
+		[TestCase(35d, null)]
+		[TestCase(35d, 1)]
+		[TestCase(5, 134d)]
+		[TestCase(null, null)]
+		[TestCase(5, 134)]
+		[TestCase(5u, 134u)]
+		public void IsNull(object lat, object lon) {
+			var converter = new LatLongToLocationConverter();
+			Assert.IsNull(converter.Convert(new[] { lat, lon }, typeof(bool), null, CultureInfo.InvariantCulture));
+
+
+		}
+
+		[TestCase(35d, 134d)]
+		[TestCase(34.9384983, 135.9120257)]
+		[TestCase(20000d, 190d)]
+		[TestCase(0d, 0d)]
+		[TestCase(-5d, -51d)]
+		[TestCase(180d, 180d)]
+		public void ConvertBack(double lat, double lon) {
+			var converter = new LatLongToLocationConverter();
+			CollectionAssert.AreEqual(
+				new[] { lat, lon },
+				converter.ConvertBack(new Location(lat, lon), new[] { typeof(bool) }, null, CultureInfo.InvariantCulture)
+			);
+		}
+	}
+}
