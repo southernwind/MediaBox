12822439608e7f9daf7fa74d16edea7252231ee6
akina <testakina@gmail.com>
2018-12-09 13:23:12 +0900

    Getのユニットテストを追加

---
+++ Tests/MediaBox.Tests/Utilities/GetTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using NUnit.Framework;
+using SandBeige.MediaBox.Composition.Settings;
+using SandBeige.MediaBox.DataBase;
+using SandBeige.MediaBox.Models.Media;
+using SandBeige.MediaBox.Models.Settings;
+using SandBeige.MediaBox.Repository;
+using SandBeige.MediaBox.Utilities;
+using Unity;
+using Unity.Lifetime;
+
+namespace SandBeige.MediaBox.Tests.Utilities {
+	[TestFixture]
+	internal class GetTest {
+		[Test]
+		public void Instance() {
+			TypeRegistrations.RegisterType(new UnityContainer());
+			var settings = Get.Instance<ISettings>();
+			// DataBase
+			var scsb = new SqliteConnectionStringBuilder {
+				DataSource = settings.PathSettings.DataBaseFilePath.Value
+			};
+			var dbContext = new MediaBoxDbContext(new SqliteConnection(scsb.ConnectionString));
+			UnityConfig.UnityContainer.RegisterInstance(dbContext, new ContainerControlledLifetimeManager());
+			dbContext.Database.EnsureDeleted();
+			dbContext.Database.EnsureCreated();
+			Assert.That(settings is Settings);
+			Assert.AreEqual("file.png",Get.Instance<MediaFile>(@"C:\file.png").FileName.Value);
+		}
+	}
+}
