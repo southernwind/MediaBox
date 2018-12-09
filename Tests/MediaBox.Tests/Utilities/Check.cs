25ede7a95e4ea07583469733574855b6eac1d118
akina <testakina@gmail.com>
2018-12-09 12:12:19 +0900

    Checkのユニットテストを追加

---
+++ Tests/MediaBox.Tests/Utilities/Check.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SandBeige.MediaBox.Composition.Settings;
+using SandBeige.MediaBox.Repository;
+using SandBeige.MediaBox.Utilities;
+using Unity;
+
+namespace SandBeige.MediaBox.Tests.Utilities {
+	[TestFixture]
+	internal class Check {
+
+		[TestCase(false, "")]
+		[TestCase(false, @"c:\image.jpeg")]
+		[TestCase(true, @"c:\image.jpg")]
+		[TestCase(false, @"c:\image.mp3")]
+		[TestCase(true, @"c:\image.mp4")]
+		[TestCase(true, @"c:\image.abc")]
+		[TestCase(true, @"file.mp4")]
+		public void IsTargetExtension(bool result,string path) {
+			TypeRegistrations.RegisterType(new UnityContainer());
+			var settings = Get.Instance<ISettings>();
+			settings.GeneralSettings.TargetExtensions.Value = new[] {".jpg", ".mp4", ".abc"};
+			Assert.AreEqual(result, path.IsTargetExtension());
+		}
+	}
+}
