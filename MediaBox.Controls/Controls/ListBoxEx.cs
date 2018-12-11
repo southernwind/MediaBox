86b200dd9ca3080462656a893b1fc908663d4593
akina <testakina@gmail.com>
2018-12-11 21:51:12 +0900

    ListBoxExにBindableSelectedItemsプロパティを追加

--- MediaBox.Controls/Controls/ListBoxEx.cs
+++ MediaBox.Controls/Controls/ListBoxEx.cs
@@ -1,13 +1,63 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using SandBeige.MediaBox.Controls.Behaviors;
 
 namespace SandBeige.MediaBox.Controls.Controls {
 	public class ListBoxEx :ListBox{
+		#region BindableSelectedItems
+		/// <summary>
+		/// バインド可能選択中アイテム 依存関係プロパティ
+		/// </summary>
+		public static readonly DependencyProperty BindableSelectedItemsProperty =
+			DependencyProperty.Register(nameof(BindableSelectedItems),
+				typeof(INotifyCollectionChanged),
+				typeof(ListBoxEx));
+
+		/// <summary>
+		/// バインド可能選択中アイテム CLR用
+		/// </summary>
+		public INotifyCollectionChanged BindableSelectedItems {
+			get {
+				return (INotifyCollectionChanged)this.GetValue(BindableSelectedItemsProperty);
+			}
+			set {
+				this.SetValue(BindableSelectedItemsProperty, value);
+			}
+		}
+		
+		/// <summary>
+		/// ListBoxExのコレクション変更時
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void OnSelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if (this.BindableSelectedItems == null) {
+				return;
+			}
+
+			foreach (var item in e.AddedItems) {
+				((IList)this.BindableSelectedItems).Add((dynamic)item);
+			}
+			foreach (var item in e.RemovedItems) {
+				((IList)this.BindableSelectedItems).Remove((dynamic)item);
+			}
+		}
+
+		#endregion
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
 		public ListBoxEx() {
+			this.SelectionChanged += this.OnSelectionChanged;
 			this.SelectionChanged += (sender, e) => {
 				this.ScrollIntoView(this.SelectedItem);
 			};
