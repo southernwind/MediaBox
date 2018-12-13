5e6eba8d210857ee71c336b4793569a60e3696bf
akina <testakina@gmail.com>
2018-12-13 22:48:03 +0000

    ApplyOrientationConverterとPathAndSizeToDecodedImageConverterをDecodeImageConverterに集約

--- MediaBox.Controls/Converters/DecodeImageConverter.cs
+++ MediaBox.Controls/Converters/DecodeImageConverter.cs
@@ -1,30 +1,47 @@
 ﻿using System;
 using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace SandBeige.MediaBox.Controls.Converters {
-	public class ApplyOrientationConverter : IMultiValueConverter {
+	/// <summary>
+	/// パスとサイズからリサイズ後画像に変換
+	/// </summary>
+	public class DecodeImageConverter : IMultiValueConverter {
+		/// <summary>
+		/// パス & サイズ→リサイズ後画像
+		/// </summary>
+		/// <param name="values">
+		/// <list>
+		/// <item>[0] = ファイルパス(string)</item>
+		/// <item>[1] = 画像の回転(int?)</item>
+		/// <item>[2] = 幅(double)</item>
+		/// <item>[3] = 高さ(double)</item>
+		/// </list>
+		/// </param>
+		/// <param name="targetType">未使用</param>
+		/// <param name="parameter">未使用</param>
+		/// <param name="culture">未使用</param>
+		/// <returns></returns>
 		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
-			var path = values[0] as string;
-			var stream = values[0] as Stream;
-			if ((path ?? (object)stream) == null) {
+			if (!(values[0] is string path)) {
 				return null;
 			}
-
 			var orientation = values[1] as int?;
+			
 			var image = new BitmapImage();
 			image.BeginInit();
-			if (path != null) {
-				image.UriSource = new Uri(path);
-			} else {
-				image.StreamSource = stream;
-			}
+			image.UriSource = new Uri(path);
 			image.CacheOption = BitmapCacheOption.OnLoad;
 			image.CreateOptions = BitmapCreateOptions.None;
+			if (values.Length == 4) {
+				if (values[2] is double width && values[3] is double height) {
+					image.DecodePixelWidth = (int)width;
+					image.DecodePixelHeight = (int)height;
+				}
+			}
 			switch (orientation) {
 				case null:
 				case 1:
