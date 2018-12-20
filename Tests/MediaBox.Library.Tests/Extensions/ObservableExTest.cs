f83a8b8ad22ba35981ffc07faa0938f0c482d117
akina <testakina@gmail.com>
2018-12-21 07:15:21 +0900

    ObservableExのユニットテストを追加

---
+++ Tests/MediaBox.Library.Tests/Extensions/ObservableExTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Reactive.Subjects;
+using NUnit.Framework;
+using SandBeige.MediaBox.Library.Extensions;
+using static SandBeige.MediaBox.Library.Extensions.ObservableEx;
+
+namespace SandBeige.MediaBox.Library.Tests.Extensions {
+	[TestFixture]
+	internal class ObservableExTest {
+		[Test]
+		public void ToOldAndNewValue() {
+			var subject = new Subject<int>();
+			var list = new List<OldAndNewValue<int>>();
+			subject.ToOldAndNewValue().Subscribe(x=> {
+				list.Add(x);
+			});
+			subject.OnNext(5);
+			Assert.AreEqual(0, list.Count);
+
+			subject.OnNext(15);
+			Assert.AreEqual(1, list.Count);
+			Assert.AreEqual(5, list[0].OldValue);
+			Assert.AreEqual(15, list[0].NewValue);
+
+			subject.OnNext(40);
+			Assert.AreEqual(2, list.Count);
+			Assert.AreEqual(15, list[1].OldValue);
+			Assert.AreEqual(40, list[1].NewValue);
+		}
+	}
+}
