ec6edf6e86f61ae0376f9e1d5c3ac3f2128e5231
akina <testakina@gmail.com>
2018-12-22 17:05:50 +0900

    MapControl,Pinのユニットテストを追加

---
+++ Tests/MediaBox.Tests/Models/Map/PinTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SandBeige.MediaBox.Models.Map;
+using SandBeige.MediaBox.Tests.Views;
+
+namespace SandBeige.MediaBox.Tests.Models.Map {
+	[TestFixture, Apartment(ApartmentState.STA)]
+	internal class PinTest : ViewTestClassBase {
+		[Test]
+		public void Test() {
+			_ = new Pin();
+		}
+	}
+}
