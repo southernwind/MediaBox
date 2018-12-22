a82061449a24181189da4308554f8f2e2aa724d8
akina <testakina@gmail.com>
2018-12-22 17:56:47 +0900

    MediaGroupViewModelのテストを移動

---
+++ Tests/MediaBox.Tests/ViewModels/Map/MediaGroupViewModelTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using SandBeige.MediaBox.Library.Map;
+using SandBeige.MediaBox.Models.Map;
+using SandBeige.MediaBox.Models.Media;
+using SandBeige.MediaBox.Utilities;
+using SandBeige.MediaBox.ViewModels.Map;
+
+namespace SandBeige.MediaBox.Tests.ViewModels.Map {
+	[TestFixture]
+	internal class MediaGroupViewModelTest : ViewModelTestClassBase {
+		[Test]
+		public void Core() {
+			var core = Get.Instance<MediaFile>("");
+			var model = Get.Instance<MediaGroup>(core, default(Rectangle));
+			var vm = Get.Instance<MediaGroupViewModel>(model);
+			vm.Core.Value.Model.Is(core);
+		}
+	}
+}
