afcf102e6be7ebbbe569bc5ba712cc827c5a64c3
akina <testakina@gmail.com>
2018-12-22 14:45:35 +0900

    MediaGroupのユニットテストを追加

---
+++ Tests/MediaBox.Tests/Models/Map/MediaGroupTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using NUnit.Framework;
+using SandBeige.MediaBox.Library.Map;
+using SandBeige.MediaBox.Models.Map;
+using SandBeige.MediaBox.Models.Media;
+using SandBeige.MediaBox.Utilities;
+
+namespace SandBeige.MediaBox.Tests.Models.Map {
+	[TestFixture]
+	internal class MediaGroupTest : TestClassBase{
+		[Test]
+		public void Test() {
+			var image1 = Get.Instance<MediaFile>(Path.Combine(TestDirectories["0"], "image1.jpg"));
+			var rect = new Rectangle(new Point(5, 15),new Size(10,8));
+			var mg = Get.Instance<MediaGroup>(image1, rect);
+
+			mg.Core.Value.Is(image1);
+			mg.CoreRectangle.Is(rect);
+			mg.Items.Count.Is(1);
+			mg.Items[0].Is(image1);
+		}
+	}
+}
