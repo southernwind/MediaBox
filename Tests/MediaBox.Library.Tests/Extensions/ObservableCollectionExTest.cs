2a3c10c039ea737c53d5e66046f1256b1bcced5e
akina <testakina@gmail.com>
2018-12-28 18:08:47 +0900

    ObservableCollectionExのユニットテスト、ReadOnlyとReadOnlyじゃないクラスのテストを共通化できそうなので共通化

--- Tests/MediaBox.Library.Tests/Extensions/ObservableCollectionExTest.cs
+++ Tests/MediaBox.Library.Tests/Extensions/ObservableCollectionExTest.cs
@@ -10,13 +10,21 @@ using SandBeige.MediaBox.Library.Extensions;
 namespace SandBeige.MediaBox.Library.Tests.Extensions {
 	[TestFixture]
 	internal class ObservableCollectionExTest {
-		[Test]
-		public void SynchronizedTo() {
+		[TestCase(true)]
+		[TestCase(false)]
+		public void SynchronizedTo(bool readOnly) {
 			var collection = new ObservableCollection<int>();
 			var target = new List<int>();
 			var target2 = new List<int>();
-			var disposable = collection.SynchronizeTo(target);
-			collection.SynchronizeTo(target2, x => x * 2);
+			IDisposable disposable;
+			if (readOnly) {
+				var roCollection = new ReadOnlyObservableCollection<int>(collection);
+				disposable = roCollection.SynchronizeTo(target);
+				roCollection.SynchronizeTo(target2, x => x * 2);
+			} else {
+				disposable = collection.SynchronizeTo(target);
+				collection.SynchronizeTo(target2, x => x * 2);
+			}
 
 			collection.Add(5);
 
@@ -73,73 +81,9 @@ namespace SandBeige.MediaBox.Library.Tests.Extensions {
 			target2.Count.Is(6);
 		}
 
-
-		[Test]
-		public void SynchronizedToReadOnly() {
-			var sourceCollection = new ObservableCollection<int>();
-			var roCollection = new ReadOnlyObservableCollection<int>(sourceCollection);
-			var target = new List<int>();
-			var target2 = new List<int>();
-			var disposable = roCollection.SynchronizeTo(target);
-			roCollection.SynchronizeTo(target2, x => x * 2);
-
-			sourceCollection.Add(5);
-
-			target.Count.Is(1);
-			target2.Count.Is(1);
-			target[0].Is(5);
-			target2[0].Is(10);
-
-			sourceCollection.Add(30);
-
-			target.Count.Is(2);
-			target2.Count.Is(2);
-			target[1].Is(30);
-			target2[1].Is(60);
-
-			sourceCollection.Remove(5);
-
-			target.Count.Is(1);
-			target2.Count.Is(1);
-			target[0].Is(30);
-			target2[0].Is(60);
-
-			sourceCollection.Add(8);
-			sourceCollection.Add(50);
-			sourceCollection.Add(100);
-
-			target.Count.Is(4);
-			target2.Count.Is(4);
-
-			sourceCollection.Clear();
-
-			target.Count.Is(0);
-			target2.Count.Is(0);
-
-			sourceCollection.AddRange(new[] { 1, 2, 3, 4, 5 });
-			target.Count.Is(5);
-			target2.Count.Is(5);
-			target.Is(new[] { 1, 2, 3, 4, 5 });
-			target2.Is(new[] { 2, 4, 6, 8, 10 });
-
-			sourceCollection[3] = 18;
-			target.Is(new[] { 1, 2, 3, 18, 5 });
-			target2.Is(new[] { 2, 4, 6, 36, 10 });
-
-			sourceCollection.Move(3, 1);
-			target.Is(new[] { 1, 18, 2, 3, 5 });
-			target2.Is(new[] { 2, 36, 4, 6, 10 });
-
-			disposable.Dispose();
-
-			sourceCollection.Add(3);
-
-			target.Count.Is(5);
-			target2.Count.Is(6);
-		}
-
-		[Test]
-		public void DisposeWhenRemove() {
+		[TestCase(true)]
+		[TestCase(false)]
+		public void DisposeWhenRemove(bool readOnly) {
 			var collection = new ObservableCollection<Disposable>();
 
 			var d1 = new Disposable();
@@ -152,7 +96,13 @@ namespace SandBeige.MediaBox.Library.Tests.Extensions {
 			collection.Select(x => x.Disposed).Is(Enumerable.Repeat(false, 5));
 			collection.Remove(d2);
 			d2.Disposed.IsFalse();
-			var dwr = collection.DisposeWhenRemove();
+			IDisposable disposable;
+			if (readOnly) {
+				var roCollection = new ReadOnlyObservableCollection<Disposable>(collection);
+				disposable = roCollection.DisposeWhenRemove();
+			} else {
+				disposable = collection.DisposeWhenRemove();
+			}
 			collection.Add(d6);
 			collection.Add(d6);
 			collection.Remove(d3);
@@ -161,37 +111,8 @@ namespace SandBeige.MediaBox.Library.Tests.Extensions {
 			collection.Remove(d4);
 			d4.Disposed.IsTrue();
 			// 解除
-			dwr.Dispose();
+			disposable.Dispose();
 			collection.Remove(d5);
-			d5.Disposed.IsFalse();
-		}
-
-		[Test]
-		public void DisposeWhenRemoveReadOnly() {
-			var sourceCollection = new ObservableCollection<Disposable>();
-			var roCollection = new ReadOnlyObservableCollection<Disposable>(sourceCollection);
-
-			var d1 = new Disposable();
-			var d2 = new Disposable();
-			var d3 = new Disposable();
-			var d4 = new Disposable();
-			var d5 = new Disposable();
-			var d6 = new Disposable();
-			sourceCollection.AddRange(new[] { d1, d2, d3, d4, d5 });
-			sourceCollection.Select(x => x.Disposed).Is(Enumerable.Repeat(false, 5));
-			sourceCollection.Remove(d2);
-			d2.Disposed.IsFalse();
-			var dwr = roCollection.DisposeWhenRemove();
-			sourceCollection.Add(d6);
-			d6.Disposed.IsFalse();
-			sourceCollection.Remove(d3);
-			d3.Disposed.IsTrue();
-			d4.Disposed.IsFalse();
-			sourceCollection.Remove(d4);
-			d4.Disposed.IsTrue();
-			// 解除
-			dwr.Dispose();
-			sourceCollection.Remove(d5);
 			d5.Disposed.IsFalse();
 		}
 	}
