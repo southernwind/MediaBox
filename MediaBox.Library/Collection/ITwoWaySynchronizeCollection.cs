9a5d3d38d8c67253c828be285697eccc058c7e0a
akina <testakina@gmail.com>
2019-01-03 21:54:24 +0900

    双方向同期するためのコレクションを追加

---
+++ MediaBox.Library/Collection/ITwoWaySynchronizeCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace SandBeige.MediaBox.Library.Collection {
+
+
+	public interface ITwoWaySynchronizeCollection<T> : INotifyCollectionChanged, IList<T> {
+		IObservable<IEnumerable<T>> AddRequestForCollectionChange {
+			get;
+		}
+		IObservable<IEnumerable<T>> RemoveRequestForCollectionChange {
+			get;
+		}
+
+		void AddRequest(IEnumerable<T> items);
+
+		void RemoveRequest(IEnumerable<T> items);
+	}
+}
