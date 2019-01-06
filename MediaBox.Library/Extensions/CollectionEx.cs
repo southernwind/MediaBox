1b11665ad03b6ddb9f1122e8ab0b0a548ed3afb0
akina <testakina@gmail.com>
2019-01-06 22:05:24 +0900

    RemoveRangeメソッドを追加。また、sourceの追加、削除をすることでitemsが変化する可能性を考慮してitemsをToArrayしておく。

--- MediaBox.Library/Extensions/CollectionEx.cs
+++ MediaBox.Library/Extensions/CollectionEx.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace SandBeige.MediaBox.Library.Extensions {
 	public static class CollectionEx {
 		public static void AddRange<T>(this ICollection<T> source, IEnumerable<T> items) {
-			foreach (var item in items) {
+			foreach (var item in items.ToArray()) {
 				source.Add(item);
+			}
+		}
+
+		public static void RemoveRange<T>(this ICollection<T> source, IEnumerable<T> items) {
+			foreach (var item in items.ToArray()) {
+				source.Remove(item);
 			}
 		}
 	}
