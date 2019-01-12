5e5fbb0a19770fdc4fbc5be6807f2e969cdfdc0b
akina <testakina@gmail.com>
2019-01-12 20:44:09 +0900

    リファクタリング VM切替時にMediaViewの子コントロールを再作成しない

--- MediaBox.Controls/Controls/ListBoxEx.cs
+++ MediaBox.Controls/Controls/ListBoxEx.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace SandBeige.MediaBox.Controls.Controls {
 	public class ListBoxEx : ListBox {
@@ -17,5 +19,22 @@ namespace SandBeige.MediaBox.Controls.Controls {
 			};
 		}
 
+		/// <summary>
+		/// ItemsSourceの変化時
+		/// </summary>
+		/// <param name="oldValue">変更前値</param>
+		/// <param name="newValue">変更後値</param>
+		protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue) {
+			// スクロール位置をリセット
+			if (this.VisualChildrenCount != 0) {
+				if (VisualTreeHelper.GetChild(this, 0) is Border border) {
+					if (border.Child is ScrollViewer scrollViewer) {
+						scrollViewer.ScrollToTop();
+					}
+				}
+			}
+
+			base.OnItemsSourceChanged(oldValue, newValue);
+		}
 	}
 }
