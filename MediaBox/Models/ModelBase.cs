0d0c7e48bd8560a4ad23d19869b8613d0e8a7330
akina <testakina@gmail.com>
2019-01-13 18:00:24 +0900

    バッキングフィールドを定義するのが面倒なのでパフォーマンスが気にならない場所ではこれでいく。

--- MediaBox/Models/ModelBase.cs
+++ MediaBox/Models/ModelBase.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using System.Runtime.CompilerServices;
 
 using Livet;
 
@@ -16,6 +18,7 @@ namespace SandBeige.MediaBox.Models {
 
 		private LivetCompositeDisposable _compositeDisposable;
 		private readonly Subject<Unit> _onDisposed = new Subject<Unit>();
+		private readonly ConcurrentDictionary<string, object> _backingFields = new ConcurrentDictionary<string, object>();
 
 		protected ModelBase() {
 			// 具象クラスのコンストラクタで使用することもあるため、属性付与によるプロパティインジェクションでは生成タイミングが遅すぎる
@@ -65,6 +68,44 @@ namespace SandBeige.MediaBox.Models {
 		/// </summary>
 		protected MediaBoxDbContext DataBase {
 			get;
+		}
+
+		/// <summary>
+		/// バッキングフィールドから値を取得(Boxingが発生するのでパフォーマンスが重要な場面では使わない)
+		/// </summary>
+		/// <typeparam name="T">型</typeparam>
+		/// <param name="member">メンバー名</param>
+		/// <returns>値</returns>
+		protected T GetValue<T>([CallerMemberName] string member = "") {
+			return this.GetValue<T>(() => default, member);
+		}
+
+		/// <summary>
+		/// バッキングフィールドから値を取得(Boxingが発生するのでパフォーマンスが重要な場面では使わない)
+		/// </summary>
+		/// <typeparam name="T">型</typeparam>
+		/// <param name="valueFactory">バッキングフィールドに値がなかった場合の値生成関数</param>
+		/// <param name="member">メンバー名</param>
+		/// <returns>値</returns>
+		protected T GetValue<T>(Func<T> valueFactory, [CallerMemberName] string member = "") {
+			return
+				(T)this
+					._backingFields
+					.GetOrAdd(member, _ => valueFactory());
+		}
+
+		/// <summary>
+		/// バッキングフィールドに値を設定(Boxingが発生するのでパフォーマンスが重要な場面では使わない)
+		/// </summary>
+		/// <typeparam name="T">型</typeparam>
+		/// <param name="value">値</param>
+		/// <param name="member">メンバー名</param>
+		protected void SetValue<T>(T value, [CallerMemberName] string member = "") {
+			if (this.GetValue<T>(member)?.Equals(value) ?? value == null) {
+				return;
+			}
+			this._backingFields[member] = value;
+			this.RaisePropertyChanged(member);
 		}
 
 		public void Dispose() {
