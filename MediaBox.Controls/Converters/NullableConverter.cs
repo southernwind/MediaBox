de0a9664c1679c8ff822890509078187f39929c8
akina <testakina@gmail.com>
2019-02-03 12:25:57 +0900

    テキストボックスを空白にした場合、VMにはnullを代入する

---
+++ MediaBox.Controls/Converters/NullableConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SandBeige.MediaBox.Controls.Converters {
+	public class NullableConverter : IValueConverter {
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+			return value;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+			if (targetType.GetGenericTypeDefinition() == typeof(Nullable<>) &&
+				value is string text &&
+				string.IsNullOrEmpty(text)) {
+				return null;
+			}
+			return value;
+		}
+	}
+}
