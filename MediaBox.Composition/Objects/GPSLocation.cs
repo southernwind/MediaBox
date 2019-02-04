c479dc35fb4c5ca1bad3c964864b6a33d7f671e4
akina <testakina@gmail.com>
2019-02-05 08:14:58 +0900

    GPSLocationにIComparableと演算子オーバーロードを実装した

--- MediaBox.Composition/Objects/GPSLocation.cs
+++ MediaBox.Composition/Objects/GPSLocation.cs
@@ -1,8 +1,10 @@
-﻿namespace SandBeige.MediaBox.Composition.Objects {
+﻿using System;
+
+namespace SandBeige.MediaBox.Composition.Objects {
 	/// <summary>
 	/// 場所、座標クラス
 	/// </summary>
-	public class GpsLocation {
+	public class GpsLocation : IComparable<GpsLocation>, IComparable {
 		/// <summary>
 		/// 緯度
 		/// </summary>
@@ -34,6 +36,87 @@
 			this.Latitude = latitude;
 			this.Longitude = longitude;
 			this.Altitude = altitude;
+		}
+
+		public int CompareTo(GpsLocation other) {
+			var c = this.Latitude.CompareTo(other.Latitude);
+			if (c != 0) {
+				return c;
+			}
+			c = this.Longitude.CompareTo(other.Longitude);
+			if (c != 0) {
+				return c;
+			}
+			if (this.Altitude is double alt) {
+				c = alt.CompareTo(other.Altitude);
+				if (c != 0) {
+					return c;
+				}
+			} else if (other.Altitude is double alt2) {
+				c = alt2.CompareTo(this.Altitude);
+				if (c != 0) {
+					return c;
+				}
+			}
+
+			return 0;
+		}
+
+		public int CompareTo(object obj) {
+			if (obj is GpsLocation gl) {
+				return this.CompareTo(gl);
+			} else {
+				return -1;
+			}
+		}
+
+		public static bool operator ==(GpsLocation gl, GpsLocation gl2) {
+			if (gl is null && gl2 is null) {
+				return true;
+			}
+			if (gl is null || gl2 is null) {
+				return false;
+			}
+			return gl.CompareTo(gl2) == 0;
+		}
+
+		public static bool operator !=(GpsLocation gl, GpsLocation gl2) {
+			return !(gl == gl2);
+		}
+
+		public static bool operator <(GpsLocation gl, GpsLocation gl2) {
+			return gl.CompareTo(gl2) < 0;
+		}
+
+		public static bool operator >(GpsLocation gl, GpsLocation gl2) {
+			return gl.CompareTo(gl2) > 0;
+		}
+
+		public static bool operator <=(GpsLocation gl, GpsLocation gl2) {
+			return gl.CompareTo(gl2) <= 0;
+		}
+
+		public static bool operator >=(GpsLocation gl, GpsLocation gl2) {
+			return gl.CompareTo(gl2) >= 0;
+		}
+
+		public override string ToString() {
+			return $"{this.Latitude} {this.Longitude} {this.Altitude}";
+		}
+
+		public override bool Equals(object obj) {
+			if (!(obj is GpsLocation)) {
+				return false;
+			}
+
+			var loc = (GpsLocation)obj;
+			return this.Latitude == loc.Latitude &&
+				this.Longitude == loc.Longitude &&
+				this.Altitude == loc.Altitude;
+		}
+
+		public override int GetHashCode() {
+			return this.Latitude.GetHashCode() ^ this.Longitude.GetHashCode() ^ this.Altitude.GetHashCode();
 		}
 	}
 }
