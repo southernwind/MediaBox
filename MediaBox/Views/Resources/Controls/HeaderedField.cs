826b23df56f073ff2a16ca56ac7b0f676cf38d93
akina <testakina@gmail.com>
2019-02-11 10:20:35 +0900

    ヘッダ付きフィールドコントロールを追加して設定画面の各入力項目のフォーマット共通化

---
+++ MediaBox/Views/Resources/Controls/HeaderedField.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SandBeige.MediaBox.Views.Resources.Controls {
+	/// <summary>
+	/// ヘッダ付きフィールド
+	/// </summary>
+	internal class HeaderedField : HeaderedContentControl {
+		/// <summary>
+		/// ヘッダ部SharedSizeGroup 依存関係プロパティ
+		/// </summary>
+		public static readonly DependencyProperty HeaderSharedSizeGroupProperty =
+			DependencyProperty.Register(nameof(HeaderSharedSizeGroup),
+				typeof(string),
+				typeof(HeaderedField),
+				new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, (sender, e) => {
+
+				}));
+
+		/// <summary>
+		/// ヘッダ部SharedSizeGroup CLR用
+		/// </summary>
+		public string HeaderSharedSizeGroup {
+			get {
+				return (string)this.GetValue(HeaderSharedSizeGroupProperty);
+			}
+			set {
+				this.SetValue(HeaderSharedSizeGroupProperty, value);
+			}
+		}
+	}
+}
