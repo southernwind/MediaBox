21ddde76dfbbcaad906e9d59422bb38911328e80
akina <testakina@gmail.com>
2019-02-15 08:06:11 +0900

    情報パネルのスタイル作成

---
+++ MediaBox.Controls/Behaviors/StyleToLoadBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+namespace SandBeige.MediaBox.Controls.Behaviors {
+	public class ApplyStyleBehavior {
+		public static readonly DependencyProperty StyleProperty =
+			DependencyProperty.RegisterAttached("Style", typeof(object), typeof(ApplyStyleBehavior), new PropertyMetadata(null,
+				(o, e) => {
+					if (e.NewValue is Style s) {
+						if (o is ContentPresenter cp) {
+							cp.Resources.Add(s.TargetType, s);
+							if (cp.Content is FrameworkElement fe2) {
+								if (!fe2.Resources.Keys.OfType<object>().Contains(s.TargetType)) {
+									fe2.Resources.Add(s.TargetType, s);
+								}
+							}
+						}
+					}
+				}));
+
+		public static object GetStyle(DependencyObject obj) {
+			return obj.GetValue(StyleProperty);
+		}
+
+		public static void SetStyle(DependencyObject obj, object value) {
+			obj.SetValue(StyleProperty, value);
+		}
+
+	}
+}
