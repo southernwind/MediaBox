eb6e8de57f4df57f3bdee14abdc441138df879e8
akina <testakina@gmail.com>
2019-03-01 08:01:15 +0900

    バックグラウンドタスク複数同時実行

--- MediaBox/Models/TaskQueue/PriorityTaskQueue.cs
+++ MediaBox/Models/TaskQueue/PriorityTaskQueue.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Threading;
 
 using Livet;
@@ -23,36 +24,35 @@ namespace SandBeige.MediaBox.Models.TaskQueue {
 		/// </summary>
 		private readonly ObservableSynchronizedCollection<TaskAction> _taskList = new ObservableSynchronizedCollection<TaskAction>();
 
-		private int _count = 0;
-
 		// コンストラクタ
 		public PriorityTaskQueue() {
-			var lockObj = new object();
-			// Task消化
-			this._taskList
-				.ObserveAddChanged<TaskAction>()
-				.ObserveOn(ThreadPoolScheduler.Instance)
-				.Where(_ => this._count < Environment.ProcessorCount)
-				.Subscribe(_ => {
-					lock (lockObj) {
-						this._count++;
-					}
-					TaskAction ta;
-					lock (this._taskList) {
-						ta = this._taskList.OrderBy(x => x.Priority).FirstOrDefault();
-						this._taskList.Remove(ta);
-					}
-					if (ta == null) {
-						return;
-					}
-					if (ta.Token.IsCancellationRequested) {
-						return;
-					}
-					Dispatcher.CurrentDispatcher.Invoke(ta.Do, DispatcherPriority.Background);
-					lock (lockObj) {
-						this._count--;
+
+			var waitHandle = new AutoResetEvent(false);
+
+			// プロセッサ数分Taskを生成する
+			foreach (var __ in Enumerable.Range(0, Environment.ProcessorCount)) {
+				Task.Run(() => {
+					while (true) {
+						TaskAction ta;
+						lock (this._taskList) {
+							ta = this._taskList.OrderBy(x => x.Priority).FirstOrDefault();
+							this._taskList.Remove(ta);
+						}
+						if (ta == null) {
+							waitHandle.WaitOne();
+							continue;
+						}
+						if (ta.Token.IsCancellationRequested) {
+							continue;
+						}
+						Dispatcher.CurrentDispatcher.Invoke(ta.Do, PriorityToDispatcherPriority(ta.Priority));
 					}
 				});
+			}
+
+			this._taskList
+				.ObserveAddChanged<TaskAction>()
+				.Subscribe(_ => waitHandle.Set());
 		}
 
 		/// <summary>
@@ -91,6 +91,23 @@ namespace SandBeige.MediaBox.Models.TaskQueue {
 		/// <returns>イテレーター</returns>
 		IEnumerator IEnumerable.GetEnumerator() {
 			return this.GetEnumerator();
+		}
+
+		/// <summary>
+		/// 優先度変換<see cref="Priority"/>-><see cref="DispatcherPriority"/>
+		/// </summary>
+		/// <param name="priority">変換前優先度</param>
+		/// <returns>変換後優先度</returns>
+		private static DispatcherPriority PriorityToDispatcherPriority(Priority priority) {
+			switch (priority) {
+				case Priority.LoadFullImage:
+					return DispatcherPriority.Background;
+				case Priority.LoadFolderAlbumFileInfo:
+				case Priority.LoadRegisteredAlbumOnLoad:
+				case Priority.LoadRegisteredAlbumOnRegister:
+					return DispatcherPriority.ContextIdle;
+			}
+			return DispatcherPriority.ApplicationIdle;
 		}
 	}
 }
