af620887ecbec96eb25690ba8a7d2236276cea58
akina <testakina@gmail.com>
2019-03-26 00:16:08 +0900

    バックグラウンドタスク表示改善

---
+++ MediaBox/Views/MainStatusBar.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SandBeige.MediaBox.Views {
+	/// <summary>
+	/// MainStatusBar.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainStatusBar {
+		public MainStatusBar() {
+			this.InitializeComponent();
+		}
+	}
+}
