954b69d28504ae126136e282c8a611507958454b
akina <testakina@gmail.com>
2019-05-01 21:33:59 +0900

    テストユーティリティ追加

--- Tests/MediaBox.TestUtilities/RxUtility.cs
+++ Tests/MediaBox.TestUtilities/RxUtility.cs
@@ -1,5 +1,8 @@
-﻿using System.Reactive.Concurrency;
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace SandBeige.MediaBox.TestUtilities {
 	public static class RxUtility {
@@ -9,6 +12,20 @@ namespace SandBeige.MediaBox.TestUtilities {
 				are.Set();
 			});
 			are.WaitOne();
+		}
+
+		/// <summary>
+		/// 一定時間定期的に条件を確認しながら条件に合致するまで待機
+		/// </summary>
+		/// <param name="condition">条件</param>
+		/// <param name="intervalMilliseconds">確認間隔</param>
+		/// <param name="timeoutMilliseconds">タイムアウト</param>
+		public static async Task WaitPolling(Func<bool> condition, int intervalMilliseconds, int timeoutMilliseconds) {
+			await Observable
+				.Interval(TimeSpan.FromMilliseconds(intervalMilliseconds))
+				.Where(_ => condition())
+				.Timeout(TimeSpan.FromMilliseconds(timeoutMilliseconds))
+				.FirstAsync();
 		}
 	}
 }
