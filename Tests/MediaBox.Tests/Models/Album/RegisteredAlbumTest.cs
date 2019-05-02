670ebe63a9f972a022d689cd9d8c2a008eac340f
akina <testakina@gmail.com>
2019-05-02 23:13:57 +0900

    RegisteredAlbumのユニットテスト追加

--- Tests/MediaBox.Tests/Models/Album/RegisteredAlbumTest.cs
+++ Tests/MediaBox.Tests/Models/Album/RegisteredAlbumTest.cs
@@ -1,222 +1,201 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
 
 using NUnit.Framework;
 
-using SandBeige.MediaBox.Composition.Settings;
-using SandBeige.MediaBox.DataBase;
 using SandBeige.MediaBox.Models.Album;
-using SandBeige.MediaBox.Models.Media;
 using SandBeige.MediaBox.TestUtilities;
-using SandBeige.MediaBox.Utilities;
+using SandBeige.MediaBox.TestUtilities.TestData;
 
 namespace SandBeige.MediaBox.Tests.Models.Album {
 	[TestFixture]
 	internal class RegisteredAlbumTest : ModelTestClassBase {
+		private TestFiles _d1;
+		private TestFiles _dsub;
+		private TestFiles _d2;
+		public override void SetUp() {
+			base.SetUp();
+			this.UseDataBaseFile();
+			this.UseFileSystem();
+			FileUtility.Copy(this.TestDataDir, this.TestDirectories["1"], TestFileNames.Image1Jpg, TestFileNames.Image2Jpg, TestFileNames.Image3Jpg);
+			FileUtility.Copy(this.TestDataDir, this.TestDirectories["sub"], TestFileNames.Image4Png);
+			FileUtility.Copy(this.TestDataDir, this.TestDirectories["2"], TestFileNames.NoExifJpg);
+
+			this._d1 = new TestFiles(this.TestDirectories["1"]);
+			this._dsub = new TestFiles(this.TestDirectories["sub"]);
+			this._d2 = new TestFiles(this.TestDirectories["2"]);
+
+			var image1 = this.MediaFactory.Create(this._d1.Image1Jpg.FilePath);
+			var image2 = this.MediaFactory.Create(this._d1.Image2Jpg.FilePath);
+			var image3 = this.MediaFactory.Create(this._d1.Image3Jpg.FilePath);
+			var image4 = this.MediaFactory.Create(this._dsub.Image4Png.FilePath);
+			var image5 = this.MediaFactory.Create(this._d2.NoExifJpg.FilePath);
+
+			var r1 = image1.CreateDataBaseRecord();
+			var r2 = image2.CreateDataBaseRecord();
+			var r3 = image3.CreateDataBaseRecord();
+			var r4 = image4.CreateDataBaseRecord();
+			var r5 = image5.CreateDataBaseRecord();
+			this.DataBase.MediaFiles.AddRange(r1, r2, r3, r4, r5);
+			this.DataBase.SaveChanges();
+			image1.MediaFileId = r1.MediaFileId;
+			image2.MediaFileId = r2.MediaFileId;
+			image3.MediaFileId = r3.MediaFileId;
+			image4.MediaFileId = r4.MediaFileId;
+			image5.MediaFileId = r5.MediaFileId;
+		}
 
 		[Test]
-		public void Create() {
-			var db = Get.Instance<MediaBoxDbContext>();
-			using (var album1 = Get.Instance<RegisteredAlbum>())
-			using (var album2 = Get.Instance<RegisteredAlbum>()) {
-				// インスタンス生成時点では何も起きない
-				db.Albums.Count().Is(0);
-				album1.AlbumId.Value.Is(0);
+		public void アルバム作成() {
+			using var album1 = new RegisteredAlbum();
+			using var album2 = new RegisteredAlbum();
+			// インスタンス生成時点では何も起きない
+			this.DataBase.Albums.Count().Is(0);
+			album1.AlbumId.Value.Is(0);
 
-				// 作成するとDB登録される
-				album1.Create();
-				db.Albums.Count().Is(1);
-				album1.AlbumId.Value.Is(1);
+			// 作成するとDB登録される
+			album1.Create();
+			this.DataBase.Albums.Count().Is(1);
+			album1.AlbumId.Value.Is(1);
 
-				// 作成するとDB登録される
-				album2.Create();
-				db.Albums.Count().Is(2);
-				album2.AlbumId.Value.Is(2);
+			// 作成するとDB登録される
+			album2.Create();
+			this.DataBase.Albums.Count().Is(2);
+			album2.AlbumId.Value.Is(2);
+		}
+
+		[Test]
+		public void データベース情報の反映読み込み() {
+			using (var album = new RegisteredAlbum()) {
+				album.Create();
+				album.Title.Value = "タイトル";
+				album.AlbumPath.Value = @"/picture/ff";
+				album.Directories.Add(@"C:\test\");
+				album.Directories.Add(@"C:\data\");
+				album.ReflectToDataBase();
+				album.AlbumId.Value.Is(1);
+			}
+			using (var album = new RegisteredAlbum()) {
+				album.LoadFromDataBase(1);
+				album.Title.Value.Is("タイトル");
+				album.AlbumPath.Value.Is(@"/picture/ff");
+				album.Directories.OrderBy(x => x).Is(@"C:\data\", @"C:\test\");
 			}
 		}
 
 		[Test]
-		public void LoadFromDataBase() {
-			var db = Get.Instance<MediaBoxDbContext>();
-			using (var album1 = Get.Instance<RegisteredAlbum>())
-			using (var album2 = Get.Instance<RegisteredAlbum>())
-			using (var album3 = Get.Instance<RegisteredAlbum>()) {
-				db.Albums.Count().Is(0);
+		public async Task ファイル追加削除() {
+			using var image1 = this.MediaFactory.Create(this._d1.Image1Jpg.FilePath);
+			using var image2 = this.MediaFactory.Create(this._d1.Image2Jpg.FilePath);
+			using var image3 = this.MediaFactory.Create(this._d1.Image3Jpg.FilePath);
+			using var image4 = this.MediaFactory.Create(this._dsub.Image4Png.FilePath);
+			using var image5 = this.MediaFactory.Create(this._d2.NoExifJpg.FilePath);
 
-				// 存在しないアルバム
-				Assert.Catch<InvalidOperationException>(() => album1.LoadFromDataBase(1));
+			using (var album = new RegisteredAlbum()) {
+				album.Create();
+				album.AddFiles(new[] { image1, image2 });
+				await RxUtility.WaitPolling(() => album.Items.Count() >= 2, 100, 5000);
+				album.Count.Value.Is(2);
+				album.Items.Check(this._d1.Image1Jpg, this._d1.Image2Jpg);
+				this.DataBase
+					.AlbumMediaFiles
+					.Include(x => x.MediaFile)
+					.Where(x => x.AlbumId == 1)
+					.Select(x => x.MediaFile)
+					.Check(this._d1.Image1Jpg, this._d1.Image2Jpg);
 
-				album1.Items.Count.Is(0);
-				album1.Count.Value.Is(0);
-				album1.Directories.Count.Is(0);
-				album1.Title.Value.IsNull();
-				album1.AlbumPath.Value.Is("");
+				album.RemoveFiles(new[] { image1 });
+				album.Count.Value.Is(1);
+				album.Items.Check(this._d1.Image2Jpg);
+				this.DataBase
+					.AlbumMediaFiles
+					.Include(x => x.MediaFile)
+					.Where(x => x.AlbumId == 1)
+					.Select(x => x.MediaFile)
+					.Check(this._d1.Image2Jpg);
+			}
 
-				// アルバム2と3を作って登録
-				album2.Create();
-				album2.Title.Value = "album2";
-				album2.AlbumPath.Value = "/iphone/path";
-				album2.Directories.Add(TestDirectories["2"]);
-				album2.Directories.Add(TestDirectories["4"]);
-				album2.Directories.Add(TestDirectories["6"]);
-				album2.AddFiles(new[]{
-					this.MediaFactory.Create(Path.Combine(TestDataDir, "image1.jpg")),
-					this.MediaFactory.Create(Path.Combine(TestDataDir, "image2.jpg")),
-					this.MediaFactory.Create(Path.Combine(TestDataDir, "image3.jpg"))
-				});
-				album2.ReflectToDataBase();
-				db.Albums.Count().Is(1);
-				album2.AlbumId.Value.Is(1);
-
-				album3.Create();
-				album3.Title.Value = "album3";
-				album3.AlbumPath.Value = "/android/pen";
-				album3.Directories.Add(TestDirectories["3"]);
-				album3.Directories.Add(TestDirectories["5"]);
-				album3.AddFiles(new[]{
-					this.MediaFactory.Create(Path.Combine(TestDataDir, "image4.jpg")),
-					this.MediaFactory.Create(Path.Combine(TestDataDir, "image5.jpg"))
-				});
-				album3.ReflectToDataBase();
-				db.Albums.Count().Is(2);
-				album3.AlbumId.Value.Is(2);
-
-				// アルバム1にアルバム2のデータを読み込む
-				album1.LoadFromDataBase(1);
-
-				album1.AlbumId.Value.Is(1);
-				album1.Title.Value.Is("album2");
-				album1.AlbumPath.Value.Is("/iphone/path");
-				album1.Items.Count.Is(3);
-				album1.Directories.Is(
-					TestDirectories["2"],
-					TestDirectories["4"],
-					TestDirectories["6"]);
-				album1.Items.Select(x => x.FileName).OrderBy(x => x).Is(
-					"image1.jpg",
-					"image2.jpg",
-					"image3.jpg");
+			using (var album = new RegisteredAlbum()) {
+				album.LoadFromDataBase(1);
+				album.Count.Value.Is(1);
+				album.Items.Check(this._d1.Image2Jpg);
 			}
 		}
 
 		[Test]
-		public void AddRemoveFiles() {
-			using (var album1 = Get.Instance<RegisteredAlbum>())
-			using (var album2 = Get.Instance<RegisteredAlbum>())
-			using (var album3 = Get.Instance<RegisteredAlbum>()) {
-				var db = Get.Instance<MediaBoxDbContext>();
-				var image1 = (ImageFileModel)this.MediaFactory.Create(Path.Combine(TestDataDir, "image1.jpg"));
-				var image2 = (ImageFileModel)this.MediaFactory.Create(Path.Combine(TestDataDir, "image2.jpg"));
-				var image3 = (ImageFileModel)this.MediaFactory.Create(Path.Combine(TestDataDir, "image3.jpg"));
-				var image4 = (ImageFileModel)this.MediaFactory.Create(Path.Combine(TestDataDir, "image4.jpg"));
-				var image5 = (ImageFileModel)this.MediaFactory.Create(Path.Combine(TestDataDir, "image5.jpg"));
-				// アルバム3つ作成
-				album1.Create();
-				album2.Create();
-				album3.Create();
-
-				// 初期値
-				album1.Items.Count.Is(0);
-				image1.Thumbnail.IsNull();
-				db.MediaFiles.Count().Is(0);
-				db.AlbumMediaFiles.Count().Is(0);
-
-				// アルバム1に1,2,5を追加
-				album1.AddFiles(new[]{
-					image1,
-					image2,
-					image5
-				});
-				// アルバム2に3を追加
-				album2.AddFiles(new[] { image3 });
-				// アルバム3に4,5を追加
-				album3.AddFiles(new[]{
-					image4,
-					image5
-				});
-
-				// アルバムに追加されるとサムネイルとExifが読み込まれる
-				image1.Thumbnail.IsNotNull();
-				// データベースに登録されている
-				// アルバム1,3に登録されたimage5は重複登録されない
-				db.MediaFiles.Count().Is(5);
-				db.AlbumMediaFiles.Count().Is(6);
-
-				album1.Items.Count.Is(3);
-				album1.Items.OrderBy(x => x.FileName).Is(image1, image2, image5);
-				album2.Items.Count.Is(1);
-				album2.Items.Is(image3);
-				album3.Items.Count.Is(2);
-				album3.Items.OrderBy(x => x.FileName).Is(
-					image4,
-					image5);
-
-				album3.RemoveFiles(new[] { image5 });
-				album3.Items.Is(image4);
-
-				album1.RemoveFiles(new[] { image1, image2 });
-
-				album1.Items.Is(image5);
-
-				// 一度登録されたイメージは削除されない
-				db.MediaFiles.Count().Is(5);
-				// アルバムとのリレーションは解除される
-				db.AlbumMediaFiles.Count().Is(3);
+		public async Task ロードパターン1() {
+			using var image1 = this.MediaFactory.Create(this._d1.Image1Jpg.FilePath);
+			using var image2 = this.MediaFactory.Create(this._d1.Image2Jpg.FilePath);
+			using var image3 = this.MediaFactory.Create(this._d1.Image3Jpg.FilePath);
+			using var image4 = this.MediaFactory.Create(this._dsub.Image4Png.FilePath);
+			using var image5 = this.MediaFactory.Create(this._d2.NoExifJpg.FilePath);
+			using (var album = new RegisteredAlbum()) {
+				album.Create();
+				album.Directories.Add(this.TestDirectories["2"]);
+				album.ReflectToDataBase();
+				album.AddFiles(new[] { image1, image2 });
+				await RxUtility.WaitPolling(() => album.Items.Count() >= 2, 100, 5000);
 			}
+
+			var fa = new RegisteredAlbum();
+			fa.LoadFromDataBase(1);
+			await RxUtility.WaitPolling(() => fa.Items.Count() >= 3, 100, 5000);
+			fa.Items.Check(
+				this._d1.Image1Jpg,
+				this._d1.Image2Jpg,
+				this._d2.NoExifJpg);
 		}
 
 		[Test]
-		public void LoadFileInDirectoryAsync() {
-			var settings = Get.Instance<ISettings>();
-			settings.GeneralSettings.ImageExtensions.Clear();
-			settings.GeneralSettings.ImageExtensions.Add(".jpg");
-
-			FileUtility.Copy(
-				TestDataDir,
-				TestDirectories["1"],
-				new[] { "image1.jpg", "image2.jpg", "image4.jpg", "image6.jpg", "image8.jpg", "image9.png" });
-
-
-			FileUtility.Copy(
-				TestDataDir,
-				TestDirectories["sub"],
-				new[] { "image3.jpg", "image7.jpg" });
-
-
-			FileUtility.Copy(
-				TestDataDir,
-				TestDirectories["2"],
-				new[] { "image5.jpg" });
-
-			using (var album1 = Get.Instance<RegisteredAlbum>()) {
-
-				album1.Create();
-				album1.Directories.Add(TestDirectories["1"]);
-				album1.Items.Count.Is(7);
-				album1.Items.Select(x => x.FilePath).OrderBy(x => x).Is(
-					Path.Combine(TestDirectories["1"], "image1.jpg"),
-					Path.Combine(TestDirectories["1"], "image2.jpg"),
-					Path.Combine(TestDirectories["1"], "image4.jpg"),
-					Path.Combine(TestDirectories["1"], "image6.jpg"),
-					Path.Combine(TestDirectories["1"], "image8.jpg"),
-					Path.Combine(TestDirectories["sub"], "image3.jpg"),
-					Path.Combine(TestDirectories["sub"], "image7.jpg"));
-
-				album1.Directories.Add(TestDirectories["2"]);
-
-				album1.Items.Count.Is(8);
-				album1.Items.Select(x => x.FilePath).OrderBy(x => x).Is(
-					Path.Combine(TestDirectories["1"], "image1.jpg"),
-					Path.Combine(TestDirectories["1"], "image2.jpg"),
-					Path.Combine(TestDirectories["1"], "image4.jpg"),
-					Path.Combine(TestDirectories["1"], "image6.jpg"),
-					Path.Combine(TestDirectories["1"], "image8.jpg"),
-					Path.Combine(TestDirectories["sub"], "image3.jpg"),
-					Path.Combine(TestDirectories["sub"], "image7.jpg"),
-					Path.Combine(TestDirectories["2"], "image5.jpg")
-				);
+		public async Task ロードパターン2() {
+			using var image1 = this.MediaFactory.Create(this._d1.Image1Jpg.FilePath);
+			using var image2 = this.MediaFactory.Create(this._d1.Image2Jpg.FilePath);
+			using var image3 = this.MediaFactory.Create(this._d1.Image3Jpg.FilePath);
+			using var image4 = this.MediaFactory.Create(this._dsub.Image4Png.FilePath);
+			using var image5 = this.MediaFactory.Create(this._d2.NoExifJpg.FilePath);
+			using (var album = new RegisteredAlbum()) {
+				album.Create();
+				album.Directories.Add(this.TestDirectories["2"]);
+				album.Directories.Add(this.TestDirectories["sub"]);
+				album.ReflectToDataBase();
+				album.AddFiles(new[] { image1 });
+				await RxUtility.WaitPolling(() => album.Items.Count() >= 1, 100, 5000);
 			}
+
+			var fa = new RegisteredAlbum();
+			fa.LoadFromDataBase(1);
+			await RxUtility.WaitPolling(() => fa.Items.Count() >= 3, 100, 5000);
+			fa.Items.Check(
+				this._d1.Image1Jpg,
+				this._dsub.Image4Png,
+				this._d2.NoExifJpg);
+		}
+
+		[Test]
+		public async Task ロードパターン3() {
+			using var image1 = this.MediaFactory.Create(this._d1.Image1Jpg.FilePath);
+			using var image2 = this.MediaFactory.Create(this._d1.Image2Jpg.FilePath);
+			using var image3 = this.MediaFactory.Create(this._d1.Image3Jpg.FilePath);
+			using var image4 = this.MediaFactory.Create(this._dsub.Image4Png.FilePath);
+			using var image5 = this.MediaFactory.Create(this._d2.NoExifJpg.FilePath);
+			using (var album = new RegisteredAlbum()) {
+				album.Create();
+				album.Directories.Add(this.TestDirectories["2"]);
+				album.Directories.Add(this.TestDirectories["sub"]);
+				album.ReflectToDataBase();
+				album.AddFiles(new[] { image1 });
+				await RxUtility.WaitPolling(() => album.Items.Count() >= 1, 100, 5000);
+			}
+
+			var fa = new RegisteredAlbum();
+			fa.LoadFromDataBase(1);
+			await RxUtility.WaitPolling(() => fa.Items.Count() >= 3, 100, 5000);
+			fa.Items.Check(
+				this._d1.Image1Jpg,
+				this._dsub.Image4Png,
+				this._d2.NoExifJpg);
 		}
 	}
 }
