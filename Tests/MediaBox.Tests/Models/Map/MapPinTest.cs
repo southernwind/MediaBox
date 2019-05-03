294495dbc01ea6ea174379a46afae5127d888b36
akina <testakina@gmail.com>
2019-05-03 23:52:29 +0900

    マップピンのユニットテストを追加

--- Tests/MediaBox.Tests/Models/Map/MapPinTest.cs
+++ Tests/MediaBox.Tests/Models/Map/MapPinTest.cs
@@ -1,25 +1,32 @@
-﻿using System.IO;
-using System.Windows;
+﻿using System.Windows;
 
 using NUnit.Framework;
 
 using SandBeige.MediaBox.Library.Map;
 using SandBeige.MediaBox.Models.Map;
-using SandBeige.MediaBox.Utilities;
 
 namespace SandBeige.MediaBox.Tests.Models.Map {
 	[TestFixture]
 	internal class MapPinTest : ModelTestClassBase {
+		public override void SetUp() {
+			base.SetUp();
+			this.UseDataBaseFile();
+		}
+
 		[Test]
-		public void Test() {
-			var image1 = this.MediaFactory.Create(Path.Combine(TestDataDir, "image1.jpg"));
+		public void テスト() {
+			var image1 = this.MediaFactory.Create(this.TestFiles.Image1Jpg.FilePath);
+			var image2 = this.MediaFactory.Create(this.TestFiles.Image2Jpg.FilePath);
+			var image3 = this.MediaFactory.Create(this.TestFiles.Image3Jpg.FilePath);
 			var rect = new Rectangle(new Point(5, 15), new Size(10, 8));
-			var mg = Get.Instance<MapPin>(image1, rect);
+			var mg = new MapPin(image1, rect);
+			mg.Items.Add(image2);
+			mg.Items.Add(image3);
 
 			mg.Core.Value.Is(image1);
 			mg.CoreRectangle.Is(rect);
-			mg.Items.Count.Is(1);
-			mg.Items[0].Is(image1);
+			mg.Count.Value.Is(3);
+			mg.Items.Is(image1, image2, image3);
 		}
 	}
 }
