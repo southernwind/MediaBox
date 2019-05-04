0db739b25de459f7921fdda33bc42a20d11c05bf
akina <testakina@gmail.com>
2019-05-04 20:27:11 +0900

    ExternalToolのユニットテストを追加

---
+++ Tests/MediaBox.Tests/Models/Tools/ExternalToolTest.cs
@@ -0,0 +1,68 @@
+﻿
+using NUnit.Framework;
+
+using Reactive.Bindings;
+
+using SandBeige.MediaBox.Composition.Objects;
+using SandBeige.MediaBox.Library.Extensions;
+using SandBeige.MediaBox.Models.Tools;
+using SandBeige.MediaBox.TestUtilities;
+
+namespace SandBeige.MediaBox.Tests.Models.Tools {
+	internal class ExternalToolTest : ModelTestClassBase {
+		public override void SetUp() {
+			base.SetUp();
+			this.UseDataBaseFile();
+			this.UseFileSystem();
+		}
+
+		[Test]
+		public void 表示名() {
+			var param = new ExternalToolParams();
+			param.DisplayName.Value = "name";
+
+			var et = new ExternalTool(param);
+			et.DisplayName.Value.Is("name");
+
+			param.DisplayName.Value = "name2";
+			et.DisplayName.Value.Is("name2");
+		}
+
+		[Test]
+		public void コマンド() {
+			var param = new ExternalToolParams();
+			param.Command.Value = "command";
+
+			var et = new ExternalTool(param);
+			et.Command.Value.Is("command");
+
+			param.Command.Value = "command2";
+			et.Command.Value.Is("command2");
+		}
+
+		[Test]
+		public void 引数() {
+			var param = new ExternalToolParams();
+			param.Arguments.Value = "arg";
+
+			var et = new ExternalTool(param);
+			et.Arguments.Value.Is("arg");
+
+			param.Arguments.Value = "arg2";
+			et.Arguments.Value.Is("arg2");
+		}
+
+		[Test]
+		public void 対象拡張子() {
+			var param = new ExternalToolParams();
+			param.TargetExtensions.AddRange(".jpg", ".png", ".gif");
+
+			var et = new ExternalTool(param);
+			et.TargetExtensions.Is(".jpg", ".png", ".gif");
+
+			param.TargetExtensions.AddRange(".mov", ".mp4");
+			RxUtility.WaitScheduler(ReactivePropertyScheduler.Default);
+			et.TargetExtensions.Is(".jpg", ".png", ".gif", ".mov", ".mp4");
+		}
+	}
+}
