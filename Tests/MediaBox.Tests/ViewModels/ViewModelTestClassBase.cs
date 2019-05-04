2cab652a97ab856dcfcd43df816563ffcbb07aae
akina <testakina@gmail.com>
2019-05-04 21:31:15 +0900

    ViewModelTestのベースクラスの整備

--- Tests/MediaBox.Tests/ViewModels/ViewModelTestClassBase.cs
+++ Tests/MediaBox.Tests/ViewModels/ViewModelTestClassBase.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Threading;
+﻿using System.Threading;
 using System.Windows.Threading;
 
 using Livet;
@@ -9,58 +7,79 @@ using Microsoft.Data.Sqlite;
 
 using NUnit.Framework;
 
+using SandBeige.MediaBox.Composition.Logging;
 using SandBeige.MediaBox.Composition.Settings;
 using SandBeige.MediaBox.DataBase;
 using SandBeige.MediaBox.Models.Map;
 using SandBeige.MediaBox.Models.Media;
+using SandBeige.MediaBox.Models.States;
+using SandBeige.MediaBox.Models.TaskQueue;
 using SandBeige.MediaBox.Repository;
 using SandBeige.MediaBox.Tests.Implements;
+using SandBeige.MediaBox.TestUtilities;
 using SandBeige.MediaBox.Utilities;
 using SandBeige.MediaBox.ViewModels;
 
 using Unity;
 using Unity.Lifetime;
 
-using UnityContainer = Unity.UnityContainer;
-
 namespace SandBeige.MediaBox.Tests.ViewModels {
-	internal class ViewModelTestClassBase {
-		protected static string TestDataDir;
+	internal class ViewModelTestClassBase : TestClassBase {
 		protected MediaFactory MediaFactory;
 		protected ViewModelFactory ViewModelFactory;
+		protected ISettings Settings;
+		protected States States;
+		protected MediaBoxDbContext DataBase;
+		protected PriorityTaskQueue TaskQueue;
 
 		[OneTimeSetUp]
-		public virtual void OneTimeSetUp() {
-			TestDataDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"TestData\");
+		public override void OneTimeSetUp() {
+			base.OneTimeSetUp();
 			DispatcherHelper.UIDispatcher = Dispatcher.CurrentDispatcher;
 			SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
 		}
 
 		[SetUp]
-		public virtual void SetUp() {
+		public override void SetUp() {
+			base.SetUp();
 			TypeRegistrations.RegisterType(new UnityContainer());
 
+			UnityConfig.UnityContainer.RegisterType<ILogging, Logging>(new ContainerControlledLifetimeManager());
 			UnityConfig.UnityContainer.RegisterType<IMapControl, MapControlForTest>();
-			var settings = Get.Instance<ISettings>();
-			settings.Load();
-			// DataBase
-			var sb = new SqliteConnectionStringBuilder {
-				DataSource = settings.PathSettings.DataBaseFilePath.Value
-			};
-			var dbContext = new MediaBoxDbContext(new SqliteConnection(sb.ConnectionString));
-			UnityConfig.UnityContainer.RegisterInstance(dbContext, new ContainerControlledLifetimeManager());
-			dbContext.Database.EnsureDeleted();
-			dbContext.Database.EnsureCreated();
+			this.Settings = Get.Instance<ISettings>();
+			this.States = Get.Instance<States>();
+			this.Settings.Load();
+			this.Settings.ScanSettings.ScanDirectories.Clear();
+			this.UseDataBaseFile();
 			this.MediaFactory = Get.Instance<MediaFactory>();
 			this.ViewModelFactory = Get.Instance<ViewModelFactory>();
+			this.TaskQueue = Get.Instance<PriorityTaskQueue>();
+			this.TaskQueue.TaskStart();
 		}
 
 		[TearDown]
-		public virtual void TearDown() {
-			var db = Get.Instance<MediaBoxDbContext>();
-			db.Database.EnsureDeleted();
+		public override void TearDown() {
+			if (this.DataBase != null) {
+				lock (this.DataBase) {
+					this.DataBase.Database.EnsureDeleted();
+				}
+			}
 			UnityConfig.UnityContainer.Dispose();
+			base.TearDown();
 		}
 
+		/// <summary>
+		/// データベースファイル使用
+		/// </summary>
+		protected void UseDataBaseFile() {
+			// DataBase
+			var sb = new SqliteConnectionStringBuilder {
+				DataSource = this.Settings.PathSettings.DataBaseFilePath.Value
+			};
+			this.DataBase = new MediaBoxDbContext(new SqliteConnection(sb.ConnectionString));
+			UnityConfig.UnityContainer.RegisterInstance(this.DataBase, new ContainerControlledLifetimeManager());
+			this.DataBase.Database.EnsureDeleted();
+			this.DataBase.Database.EnsureCreated();
+		}
 	}
 }
