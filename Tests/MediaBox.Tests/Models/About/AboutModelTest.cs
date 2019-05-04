a06b74e8cca1513b20baf7cf841ad4b950717028
akina <testakina@gmail.com>
2019-05-04 17:20:59 +0900

    AboutModelのユニットテストを追加

---
+++ Tests/MediaBox.Tests/Models/About/AboutModelTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+using NUnit.Framework;
+
+using SandBeige.MediaBox.Models.About;
+
+namespace SandBeige.MediaBox.Tests.Models.About {
+	internal class AboutModelTest : ModelTestClassBase {
+		public override void SetUp() {
+			base.SetUp();
+			this.UseDataBaseFile();
+		}
+
+		[Test]
+		public void カレントライセンス変更() {
+			var am = new AboutModel();
+			am.CurrentLicense.Value.ProductName.Is("ChainingAssertion-NUnit");
+			am.CurrentLicense.Value.ProjectUrl.Is("https://github.com/neuecc/ChainingAssertion");
+			am.CurrentLicense.Value.LicenseType.Is("MIT");
+			Regex.IsMatch(am.LicenseText.Value, "^MIT License.*Yoshifumi Kawai", RegexOptions.Singleline).IsTrue();
+
+			am.CurrentLicense.Value = am.Licenses.First(x => x.ProductName == "log4net");
+
+			am.CurrentLicense.Value.ProductName.Is("log4net");
+			am.CurrentLicense.Value.ProjectUrl.Is("https://github.com/apache/logging-log4net");
+			am.CurrentLicense.Value.LicenseType.Is("Apache2.0");
+			Regex.IsMatch(am.LicenseText.Value, @"^ +?Apache License.*apache\.org", RegexOptions.Singleline).IsTrue();
+		}
+	}
+}
---
+++ Tests/MediaBox.Tests/Models/About/LicenseTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+using SandBeige.MediaBox.Models.About;
+
+namespace SandBeige.MediaBox.Tests.Models.About {
+	internal class LicenseTest : ModelTestClassBase {
+
+		[Test]
+		public void インスタンス作成() {
+			var l = new License("name", "https://sample.com/", "origin");
+			l.ProductName.Is("name");
+			l.ProjectUrl.Is("https://sample.com/");
+			l.LicenseType.Is("origin");
+		}
+	}
+}
