04523fecc11d82c49bff13e745713a07acb87f0c
akina <testakina@gmail.com>
2019-06-27 23:22:07 +0900

    ReaderWriterLockのラッパー作成

---
+++ MediaBox/God/DisposableLock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace SandBeige.MediaBox.God {
+	internal sealed class DisposableLock : ReaderWriterLockSlim {
+		public DisposableLock() : base() {
+		}
+
+		public DisposableLock(LockRecursionPolicy recursionPolicy) : base(recursionPolicy) {
+		}
+
+		public IDisposable DisposableEnterReadLock() {
+			this.EnterReadLock();
+			return new DisposeObject(this.ExitReadLock);
+		}
+
+		public IDisposable DisposableEnterWriteLock() {
+			this.EnterWriteLock();
+			return new DisposeObject(this.ExitWriteLock);
+		}
+
+		private class DisposeObject : IDisposable {
+			private readonly Action _disposeAction;
+			public DisposeObject(Action disposeAction) {
+				this._disposeAction = disposeAction;
+			}
+
+			public void Dispose() {
+				this._disposeAction();
+			}
+		}
+	}
+}
---
+++ Tests/MediaBox.Tests/God/DisposableLockTest.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Threading;
+
+using NUnit.Framework;
+
+using SandBeige.MediaBox.God;
+
+namespace SandBeige.MediaBox.Tests.God {
+	[TestFixture]
+	internal class DisposableLockTest {
+		[Test]
+		public void Lock() {
+			using var dl = new DisposableLock(LockRecursionPolicy.SupportsRecursion);
+			using (var rl = dl.DisposableEnterReadLock()) {
+
+			}
+			using var _ = dl.DisposableEnterWriteLock();
+		}
+	}
+}
