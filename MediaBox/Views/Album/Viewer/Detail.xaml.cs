9d493104a1cffdddbb9e21e06665e9b5419a6a11
akina <testakina@gmail.com>
2019-07-03 08:51:47 +0900

    ライブラリ表示、詳細表示、マップ表示のコードをを完全に分割

--- MediaBox/Views/Album/Viewer/Detail.xaml.cs
+++ MediaBox/Views/Album/Viewer/Detail.xaml.cs
@@ -1,10 +1,70 @@
-﻿namespace SandBeige.MediaBox.Views.Album.Viewer {
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+using SandBeige.MediaBox.ViewModels.Album;
+namespace SandBeige.MediaBox.Views.Album.Viewer {
 	/// <summary>
 	/// Detail.xaml の相互作用ロジック
 	/// </summary>
 	public partial class Detail {
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
 		public Detail() {
 			this.InitializeComponent();
+			this.DataContextChanged += (sender, e) => {
+				if (!(this.DataContext is AlbumViewModel avm)) {
+					return;
+				}
+				avm.CurrentIndex.ObserveOn(this.Dispatcher).Subscribe(x => {
+					this.SelectedItemPositionCentering();
+				});
+			};
+
+			this.ListBox.SizeChanged += (sender, e) => {
+				if (!(this.DataContext is AlbumViewModel avm)) {
+					return;
+				}
+				this.SelectedItemPositionCentering();
+			};
+		}
+		/// <summary>
+		/// 選択中アイテムのセンタリング
+		/// </summary>
+		private async void SelectedItemPositionCentering() {
+			if (VisualTreeHelper.GetChildrenCount(this.ListBox) == 0) {
+				return;
+			}
+			if (!(VisualTreeHelper.GetChild(this.ListBox, 0) is Border border)) {
+				return;
+			}
+
+			if (!(border.Child is ScrollViewer scrollViewer)) {
+				return;
+			}
+
+			if (!(this.DataContext is AlbumViewModel avm)) {
+				return;
+			}
+
+			while (scrollViewer.ScrollableWidth == 0) {
+				await Task.Delay(1);
+				continue;
+			}
+			var listBoxWidth = scrollViewer.ViewportWidth;
+			var index = this.ListBox.SelectedIndex;
+			if (index == -1) {
+				return;
+			}
+			var scrollOffset = index - (listBoxWidth / 2) + 0.5;
+			if (scrollOffset > scrollViewer.ScrollableWidth) {
+				scrollOffset = scrollViewer.ScrollableWidth;
+			}
+
+			scrollViewer.ScrollToHorizontalOffset(scrollOffset);
 		}
 	}
 }
---
+++ MediaBox/Views/Album/Viewer/Library.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+namespace SandBeige.MediaBox.Views.Album.Viewer {
+	/// <summary>
+	/// Library.xaml の相互作用ロジック
+	/// </summary>
+	public partial class Library {
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public Library() {
+			this.InitializeComponent();
+
+			this.ListBox.SizeChanged += (sender, e) => {
+				this.SelectedItemPositionCentering();
+			};
+		}
+
+		/// <summary>
+		/// 選択中アイテムのセンタリング
+		/// </summary>
+		private async void SelectedItemPositionCentering() {
+			if (VisualTreeHelper.GetChildrenCount(this.ListBox) == 0) {
+				return;
+			}
+			if (!(VisualTreeHelper.GetChild(this.ListBox, 0) is Border border)) {
+				return;
+			}
+
+			if (!(border.Child is ScrollViewer scrollViewer)) {
+				return;
+			}
+
+			while (scrollViewer.ScrollableHeight == 0) {
+				await Task.Delay(1);
+				continue;
+			}
+			var listBoxHeight = scrollViewer.ViewportHeight;
+			var index = this.ListBox.SelectedIndex;
+			var max = this.ListBox.Items.Count;
+			var adjust = listBoxHeight * ((index - (max / 2d)) / max);
+			var scrollOffset = ((double)index / max * scrollViewer.ScrollableHeight) + adjust;
+			if (scrollOffset > scrollViewer.ScrollableHeight) {
+				scrollOffset = scrollViewer.ScrollableHeight;
+			}
+
+			scrollViewer.ScrollToVerticalOffset(scrollOffset);
+		}
+	}
+}
