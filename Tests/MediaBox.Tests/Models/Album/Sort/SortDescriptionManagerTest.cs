6a134f0751fbd6da35afe5606c634d036f9ee045
southernwind <testakina@gmail.com>
2019-08-20 22:04:30 +0900

    ソート設定テスト修正

--- Tests/MediaBox.Tests/Models/Album/Sort/SortDescriptionManagerTest.cs
+++ Tests/MediaBox.Tests/Models/Album/Sort/SortDescriptionManagerTest.cs
@@ -1,10 +1,10 @@
+using System;
 using System.ComponentModel;
 using System.Linq;
 
 using NUnit.Framework;
 
-using SandBeige.MediaBox.Composition.Interfaces;
-using SandBeige.MediaBox.Composition.Objects;
+using SandBeige.MediaBox.Composition.Enum;
 using SandBeige.MediaBox.Models.Album.Sort;
 
 namespace SandBeige.MediaBox.Tests.Models.Album.Sort {
@@ -12,32 +12,49 @@ namespace SandBeige.MediaBox.Tests.Models.Album.Sort {
 
 		[Test]
 		public void 設定値読み込み() {
-			this.States.AlbumStates.SortDescriptions["main"] = new[] {
-				new SortDescriptionParams(nameof(IMediaFileModel.FileName),ListSortDirection.Descending),
-				new SortDescriptionParams(nameof(IMediaFileModel.FilePath),ListSortDirection.Ascending),
-			};
+			this.States.AlbumStates.SortConditions.SetValue(new[] {
+				new RestorableSortObject("name1", new[] {
+					new SortItemCreator(SortItemKeys.FileName),
+					new SortItemCreator(SortItemKeys.CreationTime, ListSortDirection.Descending)
+				}),
+				new RestorableSortObject("name2", new[] {
+					new SortItemCreator(SortItemKeys.LastAccessTime, ListSortDirection.Descending),
+					new SortItemCreator(SortItemKeys.FileSize, ListSortDirection.Descending)
+				})
+			});
+			this.States.AlbumStates.CurrentSortCondition["main"] = new RestorableSortObject("name1", new[] {
+					new SortItemCreator(SortItemKeys.FileName),
+					new SortItemCreator(SortItemKeys.CreationTime, ListSortDirection.Descending)
+				});
 			using var sdm = new SortDescriptionManager("main");
-			var fn = sdm.SortItems.Single(x => x.Key == nameof(IMediaFileModel.FileName));
-			var fp = sdm.SortItems.Single(x => x.Key == nameof(IMediaFileModel.FilePath));
-			var fs = sdm.SortItems.Single(x => x.Key == nameof(IMediaFileModel.FileSize));
-			fn.Enabled.IsTrue();
-			fn.Direction.Is(ListSortDirection.Descending);
-			fp.Enabled.IsTrue();
-			fp.Direction.Is(ListSortDirection.Ascending);
-			fs.Enabled.IsFalse();
-			fs.Direction.Is(ListSortDirection.Ascending);
+			sdm.SortConditions.Count.Is(2);
+			var s1 = sdm.SortConditions.First();
+			var s2 = sdm.SortConditions.Skip(1).First();
+			s1.DisplayName.Value.Is("name1");
+			s1.SortItemCreators
+				.Select(x => (x.SortItemKey, x.Direction))
+				.Is(
+					(SortItemKeys.FileName, ListSortDirection.Ascending),
+					(SortItemKeys.CreationTime, ListSortDirection.Descending));
+			s2.DisplayName.Value.Is("name2");
+			s2.SortItemCreators
+				.Select(x => (x.SortItemKey, x.Direction))
+				.Is(
+					(SortItemKeys.LastAccessTime, ListSortDirection.Descending),
+					(SortItemKeys.FileSize, ListSortDirection.Descending));
+
+			sdm.CurrentSortCondition.Value.Is(s1);
 		}
 
 		[Test]
-		public void 更新() {
+		public void ソート設定追加() {
 			using var sdm = new SortDescriptionManager("main");
 
-			var fn = sdm.SortItems.Single(x => x.Key == nameof(IMediaFileModel.FileName));
-			this.States.AlbumStates.SortDescriptions["main"].Is();
-			fn.Enabled = true;
-			this.States.AlbumStates.SortDescriptions["main"].Is(new SortDescriptionParams(nameof(IMediaFileModel.FileName), ListSortDirection.Ascending));
-			fn.Direction = ListSortDirection.Descending;
-			this.States.AlbumStates.SortDescriptions["main"].Is(new SortDescriptionParams(nameof(IMediaFileModel.FileName), ListSortDirection.Descending));
+			this.States.AlbumStates.SortConditions.SetValue(Array.Empty<RestorableSortObject>());
+			sdm.SortConditions.IsEmpty();
+			sdm.AddCondition();
+			sdm.SortConditions.Count.Is(1);
+			this.States.AlbumStates.SortConditions.Value.Is(sdm.SortConditions.First().RestorableSortObject);
 		}
 	}
 }
