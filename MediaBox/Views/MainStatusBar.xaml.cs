baf8f7196b23c29225fd60adfa5635c4d8167947
southernwind <testakina@gmail.com>
2019-08-23 21:59:39 +0900

    ポップアップウィンドウはメインウィンドウの移動のみではなく、サイズ変更にも追従させる。

--- MediaBox/Views/MainStatusBar.xaml.cs
+++ MediaBox/Views/MainStatusBar.xaml.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Reactive.Linq;
 using System.Windows;
 
 namespace SandBeige.MediaBox.Views {
@@ -17,16 +19,22 @@ namespace SandBeige.MediaBox.Views {
 		private void StatusBar_Loaded(object sender, RoutedEventArgs e) {
 			var mainWindow = Window.GetWindow(this);
 			if (mainWindow != null) {
-				// メインウィンドウの移動にポップアップウィンドウも追従する。
-				mainWindow.LocationChanged += (_, __) => {
-					// HorizontalOffsetを変更するとイベントが発生して自動的に位置を合わせてくれるみたい。
-					var tqHo = this.TaskQueuePopup.HorizontalOffset;
-					this.TaskQueuePopup.HorizontalOffset = tqHo + 0.0001;
-					this.TaskQueuePopup.HorizontalOffset = tqHo;
-					var nHo = this.NotificationPopup.HorizontalOffset;
-					this.NotificationPopup.HorizontalOffset = nHo + 0.0001;
-					this.NotificationPopup.HorizontalOffset = nHo;
-				};
+				// メインウィンドウの移動とサイズの変更にポップアップウィンドウも追従する。
+				Observable.FromEventPattern(
+					x => mainWindow.SizeChanged += x.Invoke,
+					x => mainWindow.SizeChanged -= x.Invoke)
+					.Merge(Observable.FromEventPattern(
+						x => mainWindow.LocationChanged += x.Invoke,
+						x => mainWindow.LocationChanged -= x.Invoke))
+					.Subscribe(_ => {
+						// HorizontalOffsetを変更するとイベントが発生して自動的に位置を合わせてくれるみたい。
+						var tqHo = this.TaskQueuePopup.HorizontalOffset;
+						this.TaskQueuePopup.HorizontalOffset = tqHo + 0.0001;
+						this.TaskQueuePopup.HorizontalOffset = tqHo;
+						var nHo = this.NotificationPopup.HorizontalOffset;
+						this.NotificationPopup.HorizontalOffset = nHo + 0.0001;
+						this.NotificationPopup.HorizontalOffset = nHo;
+					});
 			}
 		}
 	}
