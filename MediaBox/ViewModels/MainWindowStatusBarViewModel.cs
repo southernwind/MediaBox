c28135f69ac07bb22331cb81f68daeed953ceade
southernwind <testakina@gmail.com>
2019-09-03 21:38:22 +0900

    未読エラーがある場合、ログ表示ボタンに赤点をつける

--- MediaBox/ViewModels/MainWindowStatusBarViewModel.cs
+++ MediaBox/ViewModels/MainWindowStatusBarViewModel.cs
@@ -60,11 +60,25 @@ namespace SandBeige.MediaBox.ViewModels {
 		}
 
 		/// <summary>
+		/// 通知詳細表示/非表示
+		/// </summary>
+		public IReactiveProperty<bool> NotificationVisibility {
+			get;
+		} = new ReactiveProperty<bool>();
+
+		/// <summary>
 		/// 通知内容
 		/// </summary>
 		public IReactiveProperty<INotification> Notification {
 			get;
 		} = new ReactivePropertySlim<INotification>();
+
+		/// <summary>
+		/// 未読エラーがあるかどうか
+		/// </summary>
+		public IReactiveProperty<bool> HasUnreadError {
+			get;
+		} = new ReactivePropertySlim<bool>();
 
 		public MainWindowStatusBarViewModel() {
 			this.LogViewerViewModel = new LogViewerViewModel().AddTo(this.CompositeDisposable);
@@ -80,6 +94,9 @@ namespace SandBeige.MediaBox.ViewModels {
 
 			var notificationManager = Get.Instance<NotificationManager>();
 			notificationManager.OnNotify.Subscribe(x => {
+				if (x is Error && !this.NotificationVisibility.Value) {
+					this.HasUnreadError.Value = true;
+				}
 				this.Notification.Value = x;
 			}).AddTo(this.CompositeDisposable);
 
@@ -89,6 +106,10 @@ namespace SandBeige.MediaBox.ViewModels {
 					this.Notification.Value = null;
 				}).AddTo(this.CompositeDisposable);
 			this.NotificationVisible = this.Notification.Select(x => x != null).ToReadOnlyReactivePropertySlim().AddTo(this.CompositeDisposable);
+
+			this.NotificationVisibility.Where(x => x).Subscribe(x => {
+				this.HasUnreadError.Value = false;
+			});
 		}
 	}
 }
