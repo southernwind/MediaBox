28cd833d3e16536d0a5df70fa5e21a144de1f794
southernwind <testakina@gmail.com>
2019-09-05 11:14:01 +0900

    スタイルチェッカーにExpanderを追加

---
+++ MediaBox.StyleChecker/ViewModels/Pages/ExpanderViewModel.cs
@@ -0,0 +1,9 @@
+namespace SandBeige.MediaBox.StyleChecker.ViewModels.Pages {
+	internal class ExpanderViewModel : IPageViewModel {
+		public string Title {
+			get {
+				return "Expander";
+			}
+		}
+	}
+}
---
+++ MediaBox.StyleChecker/Views/Pages/Expander.xaml.cs
@@ -0,0 +1,10 @@
+namespace SandBeige.MediaBox.StyleChecker.Views.Pages {
+	/// <summary>
+	/// Expander.xaml の相互作用ロジック
+	/// </summary>
+	public partial class Expander {
+		public Expander() {
+			this.InitializeComponent();
+		}
+	}
+}
