2c08fb7751a6da776ef0920289bbd759456e6d81
southernwind <testakina@gmail.com>
2019-09-05 23:24:58 +0900

    StyleCheckerにVisualStudioでデバッグ時限定の例外を無視する処理を追加

--- MediaBox.StyleChecker/App.xaml.cs
+++ MediaBox.StyleChecker/App.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows;
 
 using SandBeige.MediaBox.StyleChecker.ViewModels;
@@ -8,10 +9,35 @@ namespace SandBeige.MediaBox.StyleChecker {
 	/// </summary>
 	public partial class App {
 		protected override void OnStartup(StartupEventArgs e) {
+			AppDomain.CurrentDomain.UnhandledException += this.CurrentDomain_UnhandledException;
 			this.MainWindow = new Views.MainWindow {
 				DataContext = new MainWindowViewModel()
 			};
 			this.MainWindow.ShowDialog();
+		}
+		/// <summary>
+		/// 集約エラーハンドラ
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
+			if (e.ExceptionObject is Exception ex) {
+				Console.WriteLine(ex);
+#if DEBUG
+				// TODO:vs経由でデバッグ中に終了すると毎度例外が出てしまうので、応急処置
+				if (ex.StackTrace.Contains("Microsoft.VisualStudio.DesignTools.WpfTap.Networking.ProtocolHandler.HandleMessage")) {
+					Environment.Exit(1);
+				}
+#endif
+			}
+
+			MessageBox.Show(
+				"不明なエラーが発生しました。アプリケーションを終了します。",
+				"エラー",
+				MessageBoxButton.OK,
+				MessageBoxImage.Error);
+
+			Environment.Exit(1);
 		}
 	}
 }
