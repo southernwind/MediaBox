503e7fe47a1c49c2173af2d6146dab4527c32b1f
southernwind <testakina@gmail.com>
2019-09-05 11:05:10 +0900

    スタイルチェッカーにComboBoxを追加

---
+++ MediaBox.StyleChecker/Views/Pages/ComboBox.xaml.cs
@@ -0,0 +1,10 @@
+namespace SandBeige.MediaBox.StyleChecker.Views.Pages {
+	/// <summary>
+	/// ComboBox.xaml の相互作用ロジック
+	/// </summary>
+	public partial class ComboBox {
+		public ComboBox() {
+			this.InitializeComponent();
+		}
+	}
+}
