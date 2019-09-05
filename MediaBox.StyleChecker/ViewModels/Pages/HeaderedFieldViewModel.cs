1506ed307040d3cc8b9d5575b8834864d8efd0f2
southernwind <testakina@gmail.com>
2019-09-05 21:02:57 +0900

    スタイルチェッカーにHeaderedFieldを追加

---
+++ MediaBox.StyleChecker/ViewModels/Pages/HeaderedFieldViewModel.cs
@@ -0,0 +1,9 @@
+namespace SandBeige.MediaBox.StyleChecker.ViewModels.Pages {
+	internal class HeaderedFieldViewModel : IPageViewModel {
+		public string Title {
+			get {
+				return "HeaderedField";
+			}
+		}
+	}
+}
---
+++ MediaBox.StyleChecker/Views/Pages/HeaderedField.xaml.cs
@@ -0,0 +1,10 @@
+namespace SandBeige.MediaBox.StyleChecker.Views.Pages {
+	/// <summary>
+	/// HeaderedField.xaml の相互作用ロジック
+	/// </summary>
+	public partial class HeaderedField {
+		public HeaderedField() {
+			this.InitializeComponent();
+		}
+	}
+}
