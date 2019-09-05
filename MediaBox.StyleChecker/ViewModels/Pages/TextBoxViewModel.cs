355e8b015dd5c156ab34bf389862ae631e478202
southernwind <testakina@gmail.com>
2019-09-05 23:01:45 +0900

    スタイルチェッカーにTextBoxを追加

---
+++ MediaBox.StyleChecker/ViewModels/Pages/TextBoxViewModel.cs
@@ -0,0 +1,9 @@
+namespace SandBeige.MediaBox.StyleChecker.ViewModels.Pages {
+	internal class TextBoxViewModel : IPageViewModel {
+		public string Title {
+			get {
+				return "TextBox";
+			}
+		}
+	}
+}
---
+++ MediaBox.StyleChecker/Views/Pages/TextBox.xaml.cs
@@ -0,0 +1,10 @@
+namespace SandBeige.MediaBox.StyleChecker.Views.Pages {
+	/// <summary>
+	/// TextBox.xaml の相互作用ロジック
+	/// </summary>
+	public partial class TextBox {
+		public TextBox() {
+			this.InitializeComponent();
+		}
+	}
+}
