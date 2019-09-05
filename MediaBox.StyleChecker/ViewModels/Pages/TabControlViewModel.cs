89608545c97ed08b40a33597948046fae29b3a5d
southernwind <testakina@gmail.com>
2019-09-05 22:55:59 +0900

    スタイルチェッカーにTabControlを追加

---
+++ MediaBox.StyleChecker/ViewModels/Pages/TabControlViewModel.cs
@@ -0,0 +1,9 @@
+namespace SandBeige.MediaBox.StyleChecker.ViewModels.Pages {
+	internal class TabControlViewModel : IPageViewModel {
+		public string Title {
+			get {
+				return "TabControl";
+			}
+		}
+	}
+}
