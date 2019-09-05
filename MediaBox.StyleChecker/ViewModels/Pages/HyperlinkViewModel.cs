265e94257ce2aa665372365dfb8245883134154c
southernwind <testakina@gmail.com>
2019-09-05 21:09:19 +0900

    スタイルチェッカーにHyperlinkを追加

---
+++ MediaBox.StyleChecker/ViewModels/Pages/HyperlinkViewModel.cs
@@ -0,0 +1,9 @@
+namespace SandBeige.MediaBox.StyleChecker.ViewModels.Pages {
+	internal class HyperlinkViewModel : IPageViewModel {
+		public string Title {
+			get {
+				return "Hyperlink";
+			}
+		}
+	}
+}
---
+++ MediaBox.StyleChecker/Views/Pages/Hyperlink.xaml.cs
@@ -0,0 +1,10 @@
+namespace SandBeige.MediaBox.StyleChecker.Views.Pages {
+	/// <summary>
+	/// Hyperlink.xaml の相互作用ロジック
+	/// </summary>
+	public partial class Hyperlink {
+		public Hyperlink() {
+			this.InitializeComponent();
+		}
+	}
+}
