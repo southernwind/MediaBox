bc354012ef7cc4603b4690315f87b2e1dc0698fe
southernwind <testakina@gmail.com>
2019-09-05 23:24:07 +0900

    スタイルチェッカーにTreeViewを追加

---
+++ MediaBox.StyleChecker/ViewModels/Pages/TreeViewViewModel.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+
+namespace SandBeige.MediaBox.StyleChecker.ViewModels.Pages {
+	internal class TreeViewViewModel : IPageViewModel {
+		public string Title {
+			get {
+				return "TreeView";
+			}
+		}
+		public IEnumerable<Nestable> NestableList {
+			get {
+				return new[] {
+					new Nestable("将軍",
+						new Nestable("大老"),
+						new Nestable("老中",
+							new Nestable("側衆"),
+							new Nestable("高家"),
+							new Nestable("大番頭"),
+							new Nestable("大目付"),
+							new Nestable("江戸町奉行"),
+							new Nestable("勘定奉行"),
+							new Nestable("勘定吟味役"),
+							new Nestable("関東郡代"),
+							new Nestable("作事奉行"),
+							new Nestable("道中奉行"),
+							new Nestable("宗門改"),
+							new Nestable("城代"),
+							new Nestable("町奉行"),
+							new Nestable("奉行"),
+							new Nestable("甲府勤番")),
+						new Nestable("側用人"),
+						new Nestable("奏者番"),
+						new Nestable("寺社奉行"),
+						new Nestable("京都所司代"),
+						new Nestable("大阪城代"),
+						new Nestable("若年寄",
+							new Nestable("書院番頭",
+								new Nestable("書院番組頭")),
+							new Nestable("小姓組番頭",
+								new Nestable("小姓組頭")),
+							new Nestable("目付")))
+				};
+			}
+		}
+	}
+
+	internal class Nestable {
+
+		public string Name {
+			get;
+		}
+
+		public IEnumerable<Nestable> Children {
+			get;
+		}
+
+		public Nestable(string name, params Nestable[] children) {
+			this.Name = name;
+			this.Children = children;
+		}
+	}
+}
---
+++ MediaBox.StyleChecker/Views/Pages/TreeView.xaml.cs
@@ -0,0 +1,10 @@
+namespace SandBeige.MediaBox.StyleChecker.Views.Pages {
+	/// <summary>
+	/// TreeView.xaml の相互作用ロジック
+	/// </summary>
+	public partial class TreeView {
+		public TreeView() {
+			this.InitializeComponent();
+		}
+	}
+}
