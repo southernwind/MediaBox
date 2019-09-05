811367d8e0ee9a5152d063ae4365b32f7edf3269
southernwind <testakina@gmail.com>
2019-09-05 22:47:47 +0900

    スタイルチェッカーにScrollViewerを追加

---
+++ MediaBox.StyleChecker/Views/Pages/ScrollViewer.xaml.cs
@@ -0,0 +1,10 @@
+namespace SandBeige.MediaBox.StyleChecker.Views.Pages {
+	/// <summary>
+	/// ScrollViewer.xaml の相互作用ロジック
+	/// </summary>
+	public partial class ScrollViewer {
+		public ScrollViewer() {
+			this.InitializeComponent();
+		}
+	}
+}
