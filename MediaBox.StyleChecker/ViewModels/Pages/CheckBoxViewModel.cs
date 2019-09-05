1ba08f9f0ac503ed0b44397b50da4ebb19323e17
southernwind <testakina@gmail.com>
2019-09-05 10:06:51 +0900

    スタイルチェッカーにチェックボックスを追加

---
+++ MediaBox.StyleChecker/ViewModels/Pages/CheckBoxViewModel.cs
@@ -0,0 +1,9 @@
+namespace SandBeige.MediaBox.StyleChecker.ViewModels.Pages {
+	internal class CheckBoxViewModel : IPageViewModel {
+		public string Title {
+			get {
+				return "CheckBox";
+			}
+		}
+	}
+}
---
+++ MediaBox.StyleChecker/Views/Pages/CheckBox.xaml.cs
@@ -0,0 +1,10 @@
+namespace SandBeige.MediaBox.StyleChecker.Views.Pages {
+	/// <summary>
+	/// CheckBox.xaml の相互作用ロジック
+	/// </summary>
+	public partial class CheckBox {
+		public CheckBox() {
+			this.InitializeComponent();
+		}
+	}
+}
