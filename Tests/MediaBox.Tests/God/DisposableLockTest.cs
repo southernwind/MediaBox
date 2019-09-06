b3caff82bb143b00e9b33c85bd38f0ce2dd2fe8f
southernwind <testakina@gmail.com>
2019-09-06 23:40:37 +0900

    DisposableLockのテストケース追加

--- Tests/MediaBox.Tests/God/DisposableLockTest.cs
+++ Tests/MediaBox.Tests/God/DisposableLockTest.cs
@@ -1,5 +1,7 @@
-
+using System;
+using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 
 using NUnit.Framework;
 
@@ -9,12 +11,184 @@ namespace SandBeige.MediaBox.Tests.God {
 	[TestFixture]
 	internal class DisposableLockTest {
 		[Test]
-		public void Lock() {
+		public async Task 読み取り専用ロックによる同時実行() {
 			using var dl = new DisposableLock(LockRecursionPolicy.SupportsRecursion);
-			using (var rl = dl.DisposableEnterReadLock()) {
+			var result = new List<string>();
+			var read1 = Task.Run(() => {
+				Thread.Sleep(30);
+				using var _ = dl.DisposableEnterReadLock();
+				result.Add("1-1");
+				Thread.Sleep(10);
+				result.Add("1-2");
+			});
+			var read2 = Task.Run(() => {
+				Thread.Sleep(20);
+				using var _ = dl.DisposableEnterReadLock();
+				result.Add("2-1");
+				Thread.Sleep(40);
+				result.Add("2-2");
+			});
+			var read3 = Task.Run(() => {
+				Thread.Sleep(10);
+				using var _ = dl.DisposableEnterReadLock();
+				result.Add("3-1");
+				Thread.Sleep(40);
+				result.Add("3-2");
+			});
 
-			}
-			using var _ = dl.DisposableEnterWriteLock();
+			await read1;
+			await read2;
+			await read3;
+
+			result.Is("3-1", "2-1", "1-1", "1-2", "3-2", "2-2");
+		}
+
+		[Test]
+		public async Task 書き込みロックによる排他制御() {
+			using var dl = new DisposableLock(LockRecursionPolicy.SupportsRecursion);
+			var result = new List<string>();
+			var write1 = Task.Run(() => {
+				Thread.Sleep(30);
+				using var _ = dl.DisposableEnterWriteLock();
+				result.Add("1-1");
+				Thread.Sleep(10);
+				result.Add("1-2");
+			});
+			var write2 = Task.Run(() => {
+				Thread.Sleep(20);
+				using var _ = dl.DisposableEnterWriteLock();
+				result.Add("2-1");
+				Thread.Sleep(40);
+				result.Add("2-2");
+			});
+			var write3 = Task.Run(() => {
+				Thread.Sleep(10);
+				using var _ = dl.DisposableEnterWriteLock();
+				result.Add("3-1");
+				Thread.Sleep(40);
+				result.Add("3-2");
+			});
+
+			await write1;
+			await write2;
+			await write3;
+
+			result.Is("3-1", "3-2", "2-1", "2-2", "1-1", "1-2");
+		}
+
+		[Test]
+		public async Task 読み込みロック中の書き込みロック取得不可() {
+			using var dl = new DisposableLock(LockRecursionPolicy.SupportsRecursion);
+			var result = new List<string>();
+			var read1 = Task.Run(() => {
+				Thread.Sleep(10);
+				using var _ = dl.DisposableEnterReadLock();
+				result.Add("r1-1");
+				Thread.Sleep(30);
+				result.Add("r1-2");
+			});
+			var write1 = Task.Run(() => {
+				Thread.Sleep(20);
+				using var _ = dl.DisposableEnterWriteLock();
+				result.Add("w1-1");
+				Thread.Sleep(10);
+				result.Add("w1-2");
+			});
+
+			await read1;
+			await write1;
+
+			result.Is("r1-1", "r1-2", "w1-1", "w1-2");
+		}
+
+		[Test]
+		public async Task 書き込みロック中の読み込みロック取得不可() {
+			using var dl = new DisposableLock(LockRecursionPolicy.SupportsRecursion);
+			var result = new List<string>();
+			var read1 = Task.Run(() => {
+				Thread.Sleep(20);
+				using var _ = dl.DisposableEnterReadLock();
+				result.Add("r1-1");
+				Thread.Sleep(10);
+				result.Add("r1-2");
+			});
+			var write1 = Task.Run(() => {
+				Thread.Sleep(10);
+				using var _ = dl.DisposableEnterWriteLock();
+				result.Add("w1-1");
+				Thread.Sleep(30);
+				result.Add("w1-2");
+			});
+
+			await read1;
+			await write1;
+
+			result.Is("w1-1", "w1-2", "r1-1", "r1-2");
+		}
+
+		[Test]
+		public async Task Disposeによるロックの解除() {
+			using var dl = new DisposableLock(LockRecursionPolicy.SupportsRecursion);
+			IDisposable lockObject = null;
+			var result = new List<string>();
+
+			lockObject = dl.DisposableEnterReadLock();
+			result.Add("w1");
+
+			var write2 = Task.Run(() => {
+				using var _ = dl.DisposableEnterWriteLock();
+				result.Add("w2");
+			});
+
+			Thread.Sleep(100);
+			result.Add("dispose");
+			lockObject.Dispose();
+
+			await write2;
+
+			result.Is("w1", "dispose", "w2");
+		}
+
+		[Test]
+		public void EnterReadLock() {
+			using var dl = new DisposableLock(LockRecursionPolicy.SupportsRecursion);
+#pragma warning disable 612
+			Assert.Throws<NotSupportedException>(dl.EnterReadLock);
+#pragma warning restore 612
+		}
+
+		[Test]
+		public void EnterWriteLock() {
+			using var dl = new DisposableLock(LockRecursionPolicy.SupportsRecursion);
+#pragma warning disable 612
+			Assert.Throws<NotSupportedException>(dl.EnterWriteLock);
+#pragma warning restore 612
+		}
+
+		[Test]
+		public async Task Dispose() {
+			var dl = new DisposableLock(LockRecursionPolicy.SupportsRecursion);
+			dl.Dispose();
+
+			// Dispose後に取得したロックは排他制御を行わない
+			await Task.Run(() => {
+				_ = dl.DisposableEnterWriteLock();
+			});
+			await Task.Run(() => {
+				_ = dl.DisposableEnterWriteLock();
+			});
+			await Task.Run(() => {
+				_ = dl.DisposableEnterReadLock();
+			});
+			await Task.Run(() => {
+				_ = dl.DisposableEnterWriteLock();
+			});
+			await Task.Run(() => {
+				_ = dl.DisposableEnterReadLock();
+			});
+			await Task.Run(() => {
+				_ = dl.DisposableEnterWriteLock();
+			});
 		}
 	}
 }
