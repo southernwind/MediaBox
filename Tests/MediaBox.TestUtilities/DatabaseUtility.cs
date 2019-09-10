88b4f2fa1c65a60e8b547ce168ca2a39340897e2
southernwind <testakina@gmail.com>
2019-09-10 14:55:02 +0900

    DatabaseUtilityの排他処理

--- Tests/MediaBox.TestUtilities/DatabaseUtility.cs
+++ Tests/MediaBox.TestUtilities/DatabaseUtility.cs
@@ -27,57 +27,60 @@ namespace SandBeige.MediaBox.TestUtilities {
 			int? rotation = null,
 			Position position = null
 			) {
-			var mf = new MediaFile {
-				DirectoryPath = $@"{Path.GetDirectoryName(filePath)}\",
-				FilePath = filePath,
-				MediaFileId = mediaFileId ?? 0,
-				ThumbnailFileName = thumbnailFileName ?? "abcdefg"
-			};
-			if (latitude is { } d && double.IsNaN(d)) {
-				mf.Latitude = 35.4218;
-			} else {
-				mf.Latitude = latitude;
-			}
-			if (longitude is { } d2 && double.IsNaN(d2)) {
-				mf.Longitude = 134.1291;
-			} else {
-				mf.Longitude = longitude;
-			}
-			if (altitude is { } d3 && double.IsNaN(d3)) {
-				mf.Altitude = 15.291;
-			} else {
-				mf.Altitude = altitude;
-			}
-			mf.FileSize = fileSize ?? 210511;
-			mf.Rate = rate ?? 0;
-			mf.Width = width ?? 640;
-			mf.Height = height ?? 480;
-			mf.Hash = hash ?? "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b";
-			mf.MediaFileTags = (tags ?? Array.Empty<string>()).Select(x => new MediaFileTag() { Tag = new Tag() { TagName = x } }).ToArray();
-			if (subTable.HasFlag(SubTable.Image)) {
-				mf.ImageFile = new ImageFile {
-					Orientation = orientation ?? 0
+			lock (dbContext) {
+				var mf = new MediaFile {
+					DirectoryPath = $@"{Path.GetDirectoryName(filePath)}\",
+					FilePath = filePath,
+					MediaFileId = mediaFileId ?? 0,
+					ThumbnailFileName = thumbnailFileName ?? "abcdefg"
 				};
-			}
-			if (subTable.HasFlag(SubTable.Video)) {
-				mf.VideoFile = new VideoFile {
-					Duration = duration,
-					Rotation = rotation
-				};
-			}
-			if (mf.Latitude is { } lat && mf.Longitude is { } lon) {
-				if (!dbContext.Positions.Any(x => x.Latitude == lat && x.Longitude == lon)) {
-					if (position == null) {
-						dbContext.Positions.Add(new Position() { Latitude = lat, Longitude = lon });
-					} else {
-						dbContext.Positions.Add(position);
-					}
+				if (latitude is { } d && double.IsNaN(d)) {
+					mf.Latitude = 35.4218;
+				} else {
+					mf.Latitude = latitude;
 				}
-			} else if (position != null) {
-				dbContext.Positions.Add(position);
+				if (longitude is { } d2 && double.IsNaN(d2)) {
+					mf.Longitude = 134.1291;
+				} else {
+					mf.Longitude = longitude;
+				}
+				if (altitude is { } d3 && double.IsNaN(d3)) {
+					mf.Altitude = 15.291;
+				} else {
+					mf.Altitude = altitude;
+				}
+				mf.FileSize = fileSize ?? 210511;
+				mf.Rate = rate ?? 0;
+				mf.Width = width ?? 640;
+				mf.Height = height ?? 480;
+				mf.Hash = hash ?? "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b";
+				mf.MediaFileTags = (tags ?? Array.Empty<string>()).Select(x => new MediaFileTag() { Tag = new Tag() { TagName = x } }).ToArray();
+				if (subTable.HasFlag(SubTable.Image)) {
+					mf.ImageFile = new ImageFile {
+						Orientation = orientation ?? 0
+					};
+				}
+				if (subTable.HasFlag(SubTable.Video)) {
+					mf.VideoFile = new VideoFile {
+						Duration = duration,
+						Rotation = rotation
+					};
+				}
+				if (mf.Latitude is { } lat && mf.Longitude is { } lon) {
+					if (!dbContext.Positions.Any(x => x.Latitude == lat && x.Longitude == lon)) {
+						if (position == null) {
+							dbContext.Positions.Add(new Position() { Latitude = lat, Longitude = lon });
+						} else {
+							dbContext.Positions.Add(position);
+						}
+					}
+				} else if (position != null) {
+					dbContext.Positions.Add(position);
+				}
+
+				dbContext.MediaFiles.Add(mf);
+				dbContext.SaveChanges();
 			}
-			dbContext.MediaFiles.Add(mf);
-			dbContext.SaveChanges();
 		}
 	}
 	public enum SubTable {
