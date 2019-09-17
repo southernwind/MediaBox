2bb4381d0cfdf398f689ffb09c11669a7d9bc8c3
southernwind <testakina@gmail.com>
2019-09-17 22:09:18 +0900

    表示設定ページのテストを追加

---
+++ Tests/MediaBox.Tests/Views/Settings/Pages/ViewerSettingsTest.cs
@@ -0,0 +1,7 @@
+
+using SandBeige.MediaBox.Views.Settings.Pages;
+
+namespace SandBeige.MediaBox.Tests.Views.Settings.Pages {
+	internal class ViewerSettingsTest : ViewTestClassBase<ViewerSettings> {
+	}
+}
