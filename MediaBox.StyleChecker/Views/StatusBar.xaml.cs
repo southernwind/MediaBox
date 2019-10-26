4ca68ae18dd7cf8473d04a284862ffd3c89c1bd3
southernwind <testakina@gmail.com>
2019-10-26 14:46:38 +0900

    スタイルチェッカーにステータスバーを追加

---
+++ MediaBox.StyleChecker/Views/StatusBar.xaml.cs
@@ -0,0 +1,10 @@
+namespace SandBeige.MediaBox.StyleChecker.Views {
+	/// <summary>
+	/// StatusBar.xaml の相互作用ロジック
+	/// </summary>
+	public partial class StatusBar {
+		public StatusBar() {
+			this.InitializeComponent();
+		}
+	}
+}
