586e7eed078a024c5066b0fd4114246818a0cf2d
southerndind <testakina@gmail.com>
2020-09-13 17:14:18 +0900

    サムネイル生成の高速化

--- MediaBox.Library/Image/ThumbnailCreator.cs
+++ MediaBox.Library/Image/ThumbnailCreator.cs
@@ -10,12 +10,12 @@ namespace SandBeige.MediaBox.Library.Image {
 		/// <param name="imageStream">元画像</param>
 		/// <param name="width">縮小後最大幅</param>
 		/// <param name="height">縮小後最大高さ</param>
-		/// <param name="orientation">画像反転/回転</param>
 		/// <returns>サムネイル画像</returns>
 		public static byte[] Create(Stream imageStream, int width, int height) {
 			using var ms = new MemoryStream();
 			using var mi = new MagickImage(imageStream);
-			mi.AdaptiveResize(width, height);
+			mi.AutoOrient();
+			mi.Thumbnail(width, height);
 			mi.Format = MagickFormat.Jpg;
 			mi.Write(ms);
 			return ms.ToArray();
